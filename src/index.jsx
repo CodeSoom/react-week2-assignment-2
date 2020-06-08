1719992776fa66d4aec134c2223db92a2cce1f0b
eonsang <2eonhworking@gmail.com>
2020-06-08 09:49:56 +0900

    todolist 추가 함수 handleAddTodoList 구현 완료

--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,25 @@ import React, { useState } from 'react';
 import ReactDom from 'react-dom';
 
 function App() {
+
+  const [todoListLength, setTodoListLength] = useState(0);
   const [todoList, setTodoList] = useState([]);
   const [input, setInput] = useState('');
+
+  const handleAddTodoList = () => {
+    setTodoList([...todoList, {
+      id: todoListLength,
+      text: input,
+    }]);
+    setTodoListLength(todoListLength + 1);
+    setInput('');
+  };
   return (
     <div>
       <h1>To-Do</h1>
       <div>
         <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
-        <button type="button">추가</button>
+        <button type="button" onClick={handleAddTodoList}>추가</button>
       </div>
 
       <ul style={{ marginTop: '20px' }}>
