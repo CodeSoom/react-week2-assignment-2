7672b666247cd55832c749a0b36b0ef929222cc7
eonsang <2eonhworking@gmail.com>
2020-06-08 09:44:47 +0900

    text input 을 state로 구현 완료

--- src/index.jsx
+++ src/index.jsx
@@ -1,24 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDom from 'react-dom';
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [input, setInput] = useState('');
   return (
     <div>
       <h1>To-Do</h1>
       <div>
-        <input type="text" />
+        <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
         <button type="button">추가</button>
       </div>
 
       <ul style={{ marginTop: '20px' }}>
-        <li>
-          test
-          <button type="button" style={{ marginLeft: '10px' }}>완료</button>
-        </li>
-        <li>
-          test
-          <button type="button" style={{ marginLeft: '10px' }}>완료</button>
-        </li>
         <li>
           test
           <button type="button" style={{ marginLeft: '10px' }}>완료</button>
