32a0665f28f9bfe5195415fdfcf56dbdd662ed85
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-09 16:12:33 +0900

    refactor(TodoTemplate): extract TodoAddButton
    - todo 추가 버튼을 별도의 컴포넌트로 관심사 분리

---
+++ src/components/TodoAddButton.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function TodoAddButton({ onClick }) {
+  return (
+    <button type="button" onClick={onClick}>추가</button>
+  );
+}
--- src/components/TodoTemplate.jsx
+++ src/components/TodoTemplate.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import TodoAddButton from './TodoAddButton';
+
 export default function TodoTemplate({ todoList, onClick }) {
   return (
     <div>
-      <button type="button" onClick={onClick}>추가</button>
+      <TodoAddButton onClick={onClick} />
       <ul>
         {todoList.map(({ key, data }) => <li key={key}>{data}</li>)}
       </ul>
