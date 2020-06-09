48dcd046de635621aa6d65ac88b6b045b40e04ee
daadaadaah <daadaadaah@gmail.com>
2020-06-09 16:23:36 +0900

    Refactor TodoItem component by removing ternary operator

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
+
 import TodoItem from './TodoItem';
 
 export default function TodoList({ todos, handleRemoveTodo }) {
+  if (todos.length === 0) {
+    return '할 일이 없어요!';
+  }
   return (
-    todos.length === 0
-      ? '할 일이 없어요!'
-      : (
-        <ul>
-          {todos.map((todo, index) => {
-            const key = index + 1;
-            return (
-              <TodoItem
-                key={key}
-                index={key}
-                todo={todo.todo}
-                handleRemoveTodo={handleRemoveTodo}
-              />
-            );
-          })}
-        </ul>
-      )
+    <ul>
+      {todos.map((todo, index) => {
+        const key = index + 1;
+        return (
+          <TodoItem
+            key={key}
+            index={key}
+            todo={todo.todo}
+            handleRemoveTodo={handleRemoveTodo}
+          />
+        );
+      })}
+    </ul>
   );
 }
