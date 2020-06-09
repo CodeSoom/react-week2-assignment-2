c08be47b202f6283aee7cecd17003bf83951c531
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-09 16:38:14 +0900

    feat(App): add input
    - 입력 창 추가

--- src/components/TodoTemplate.jsx
+++ src/components/TodoTemplate.jsx
@@ -6,6 +6,7 @@ import TodoList from './TodoList';
 export default function TodoTemplate({ todoList, onClick, onClickRemove }) {
   return (
     <div>
+      <input />
       <TodoAddButton onClick={onClick} />
       <TodoList todoList={todoList} onClick={onClickRemove} />
     </div>
