7308e97adee41762b1db65d2f4948507a4563a06
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-09 16:21:17 +0900

    refactor(TodoList): extract Todo
    - 리스트 안의 요소를 어떻게 그릴지 관리하는 컴포넌트 분리

---
+++ src/components/Todo.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Todo({ data }) {
+  return (
+    <li>{data}</li>
+  );
+}
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import Todo from './Todo';
+
 export default function TodoList({ todoList }) {
   return (
     <ul>
-      {todoList.map(({ key, data }) => <li key={key}>{data}</li>)}
+      {todoList.map(({ key, data }) => <Todo key={key} data={data} />)}
     </ul>
   );
 }
