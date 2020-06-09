e5e70f8e5f279fe24107a9e90a8b77158cd83d32
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-09 16:03:33 +0900

    feat(App)
    - 추가 버튼을 누르면 아무것도 안하기가 추가된다.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,27 @@ import React, { useState } from 'react';
 
 export default function App() {
   const [state, setState] = useState({
-    todoList: ['아무것도 안하기1', '아무것도 안하기2'],
+    todoList: [
+      { data: '아무것도 안하기1', key: 1 },
+      { data: '아무것도 안하기2', key: 2 },
+    ],
   });
 
   const { todoList } = state;
 
+  function handleClickAdd() {
+    setState({
+      todoList: todoList.concat(
+        { data: '아무것도 안하기', key: todoList.length + 1 },
+      ),
+    });
+  }
+
   return (
     <div>
-      <button type="button">추가</button>
+      <button type="button" onClick={handleClickAdd}>추가</button>
       <ul>
-        {todoList.map((todo) => <li>{todo}</li>)}
+        {todoList.map((todo) => <li key={todo.key}>{todo.data}</li>)}
       </ul>
     </div>
   );
