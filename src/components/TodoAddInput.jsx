c0043ec9c76da6df022c8645c6ea253b706453df
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-09 16:58:06 +0900

    feat(TodoAddInput)
    - input 창의 값을 state로 관리하도록 변경

--- src/components/TodoAddInput.jsx
+++ src/components/TodoAddInput.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function TodoAddInput() {
+  const [state, setState] = useState({
+    inputValue: '',
+  });
+
+  const { inputValue } = state;
+
+  function handleChange() {
+    setState({
+      inputValue: document.getElementById('todo-input').value,
+    });
+  }
+
   return (
-    <input id="todo-input" />
+    <input id="todo-input" value={inputValue} onChange={handleChange} />
   );
 }
