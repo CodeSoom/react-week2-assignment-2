e690d27b6c5b2ea98fde2f7d89dcf461a22b5f60
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-09 12:54:56 +0900

    feat(App)
    - Todo List를 관리할 state 정의
    - state를 화면에 렌더링

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [state, setState] = useState({
+    todoList: ['아무것도 안하기1', '아무것도 안하기2'],
+  });
+
+  const { todoList } = state;
+
   return (
     <div>
       <button type="button">추가</button>
+      <ul>
+        {todoList.map((todo) => <li>{todo}</li>)}
+      </ul>
     </div>
   );
 }
