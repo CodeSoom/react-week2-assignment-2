33f7f41c3bd969157f42c6b9f1ab66f5f7aa6e5c
Kwakcena <rhkrgudwh@gmail.com>
2020-06-10 14:32:11 +0900

    할 일 추가 기능 만들기
    할 일을 입력하고 추가하면 할 일 목록에 추가되도록 기능을 추가한다.

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,41 @@ import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
 export default function App() {
-  const [state, setState] = useState([
+  const [todos, setTodos] = useState([
     {
       id: 1,
       todo: '자바스크립트 공부',
+      checked: false,
     },
     {
       id: 2,
       todo: '리액트 공부',
+      checked: false,
     },
     {
       id: 3,
       todo: '운동하기',
+      checked: false,
     },
   ]);
-  const todos = state;
+
+  const onInsert = (value) => {
+    setTodos([
+      ...todos,
+      {
+        id: 4,
+        todo: value,
+        checked: false,
+      },
+    ]);
+  };
 
   return (
     <div>
       <h1>To-do</h1>
-      <TodoInsert />
+      <TodoInsert
+        onInsert={onInsert}
+      />
       <TodoList
         todos={todos}
       />
--- src/components/TodoInsert.jsx
+++ src/components/TodoInsert.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function TodoInsert() {
+export default function TodoInsert({ onInsert }) {
+  const [value, setValue] = useState('');
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    onInsert(value);
+    setValue('');
+    e.preventDefault();
+  };
+
   return (
-    <form>
-      <input type="text" placeholder="할 일을 입력해 주세요" />
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        placeholder="할 일을 입력해 주세요"
+        name="todo"
+        value={value}
+        onChange={handleChange}
+      />
       <button type="submit">추가</button>
     </form>
   );
