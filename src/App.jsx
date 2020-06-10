e2421b495e9929c68a5babcb901f870acad71fb9
JangGeonil <>
2020-06-10 20:00:39 +0900

    test code 작성 테스트

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Title from './components/Title';
 import TodoList from './components/TodoList';
 
 export default function App() {
+  const [state, setState] = useState({
+    todos: [],
+    value: '',
+    isEmpty: false,
+  });
+
+  const { todos, value, isEmpty } = state;
+
+  const onChange = (e) => {
+    setState({
+      ...state,
+      value: e.target.value,
+      isEmpty: false,
+    });
+  };
+
+  const onAddTodo = () => {
+    if (!value) {
+      setState({
+        ...state,
+        isEmpty: true,
+      });
+      return;
+    }
+    setState({
+      ...state,
+      todos: [...state.todos, { id: new Date().getTime(), task: value }],
+      value: '',
+    });
+  };
+
+  const onCompleteTodo = (id) => {
+    const filteredTodos = todos.filter((todo) => todo.id !== id);
+    setState({
+      ...state,
+      todos: filteredTodos,
+    });
+  };
+
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    onAddTodo();
+  };
+
   return (
     <>
       <Title title="To-do" />
-      <TodoList />
+      <TodoList
+        value={value}
+        onChange={onChange}
+        onSubmit={onSubmit}
+        isEmpty={isEmpty}
+        todos={todos}
+        onCompleteTodo={onCompleteTodo}
+      />
     </>
   );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,52 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import Todo from './Todo';
 import Form from './Form';
 
-function TodoList() {
-  const [state, setState] = useState({
-    todos: [],
-    value: '',
-    isEmpty: false,
-  });
-
-  const { todos, value, isEmpty } = state;
-
-  const onChange = (e) => setState({
-    ...state,
-    value: e.target.value,
-    isEmpty: false,
-  });
-
-  const onAddTodo = () => {
-    if (!value) {
-      setState({
-        ...state,
-        isEmpty: true,
-      });
-      return;
-    }
-    setState({
-      ...state,
-      todos: [...state.todos, { id: new Date().getTime(), task: value }],
-      value: '',
-    });
-  };
-
-  const onCompleteTodo = (id) => {
-    const filteredTodos = todos.filter((todo) => todo.id !== id);
-    setState({
-      ...state,
-      todos: filteredTodos,
-    });
-  };
-
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    onAddTodo();
-  };
-
+function TodoList({
+  value, onChange, onSubmit, isEmpty, todos, onCompleteTodo,
+}) {
   return (
     <div>
       <Form onSubmit={onSubmit} value={value} onChange={onChange} isEmpty={isEmpty} />
