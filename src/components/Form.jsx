3b32dddac06815f7de12b0aa56be382e0d9d3065
JangGeonil <>
2020-06-10 09:55:35 +0900

    등록 -> 추가로 버튼명 변경:

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -9,7 +9,7 @@ export default function Form({
     <form onSubmit={onSubmit}>
       <div>
         <Input value={value} onChange={onChange} placeholder="할 일을 적어주세요." />
-        <button type="submit">등록</button>
+        <button type="submit">추가</button>
       </div>
       {isEmpty && <b style={{ color: 'red' }}>할 일을 적어주세요.</b>}
     </form>
