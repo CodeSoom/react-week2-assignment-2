3933f65f7f903f7b18fb5790344b0a1edc989d29
Kwakcena <rhkrgudwh@gmail.com>
2020-06-10 10:40:24 +0900

    Make to-do input component
    todo list를 추가할 수 있는 input 컴포넌트를 만듬.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import TodoInsert from './components/TodoInsert';
 
 export default function App() {
   return (
     <div>
       <h1>To-do</h1>
+      <TodoInsert />
     </div>
   );
 }
---
+++ src/components/TodoInsert.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function TodoInsert() {
+  return (
+    <form>
+      <input type="text" placeholder="할 일을 입력해 주세요"></input>
+      <button type="submit">추가</button>
+    </form>
+  );
+}
