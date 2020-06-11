ad325483698ea628fe6eea50ba62538136d17300
sxhyxnchxng <sxhyxnchxng@gmail.com>
2020-06-11 21:47:12 +0900

    chore: early return EmptyMessage

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,19 +3,19 @@ import React from 'react';
 import EmptyMessage from './EmptyMessage';
 
 export default function TodoList({ todos, handleComplete }) {
+  if (!todos.length) {
+    return <EmptyMessage />;
+  }
   return (
-    !todos.length ? <EmptyMessage />
-      : (
-        <ol>
-          {todos.map((todo) => (
-            <li key={todo.id}>
-              {todo.text}
-              <button type="button" onClick={() => handleComplete(todo.id)}>
-                완료
-              </button>
-            </li>
-          ))}
-        </ol>
-      )
+    <ol>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          {todo.text}
+          <button type="button" onClick={() => handleComplete(todo.id)}>
+            완료
+          </button>
+        </li>
+      ))}
+    </ol>
   );
 }
