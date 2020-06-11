1933e36a9a4889590ca03c752ca763539820be17
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-11 11:04:49 +0900

    refactor(TodoList)
    - 더 나은 가독성을 위해 3항 연산자 개선
    - 특정 상황(배열이 비었을 때) 렌더링하는 관심사를 독립적으로 분리

--- src/components/TodoBody/TodoList.jsx
+++ src/components/TodoBody/TodoList.jsx
@@ -3,14 +3,18 @@ import React from 'react';
 import Todo from './Todo';
 
 export default function TodoList({ todoList, onClick }) {
+  if (todoList.length === 0) {
+    return (
+      <div>할 일이 없어요</div>
+    );
+  }
+
   return (
     <ul>
       {
-        (todoList.length === 0)
-          ? '할 일이 없어요!'
-          : todoList.map(
-            ({ data, key }) => <Todo key={key} id={key} data={data} onClick={onClick} />,
-          )
+        todoList.map(
+          ({ data, key }) => <Todo key={key} id={key} data={data} onClick={onClick} />,
+        )
       }
     </ul>
   );
