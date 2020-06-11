4df97fd293ca1cb2887336dceeaf6f75a36691da
eonsang <2eonhworking@gmail.com>
2020-06-11 22:11:23 +0900

    state, props 이동

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDom from 'react-dom';
 
 import Todo from './pages/Todo';
 
 function App() {
-  return <Todo />;
+  const [state, setState] = useState({
+    todoList: [],
+    input: '',
+    id: 0,
+  });
+  const { todoList, input } = state;
+
+  const handleChangeInput = ({ target: { value } }) => {
+    setState({
+      ...state,
+      input: value,
+    });
+  };
+
+  const handleClickAddTodo = () => {
+    if (input.trim() !== '') {
+      setState((prevState) => ({
+        ...state,
+        input: '',
+        id: prevState.id + 1,
+        todoList: [
+          ...todoList,
+          {
+            id: state.id,
+            text: input,
+          },
+        ],
+      }));
+    }
+  };
+
+  const handleClickRemoveTodo = (id) => {
+    setState({
+      ...state,
+      todoList: todoList.filter((item) => item.id !== id),
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleClickAddTodo();
+  };
+
+  return (
+    <Todo
+      todoList={todoList}
+      input={input}
+      handleChangeInput={handleChangeInput}
+      handleClickAddTodo={handleClickAddTodo}
+      handleClickRemoveTodo={handleClickRemoveTodo}
+      handleSubmit={handleSubmit}
+    />
+  );
 }
 
 ReactDom.render(<App />, document.getElementById('app'));
--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,52 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import TodoForm from '../components/TodoForm';
 import TodoList from '../components/TodoList';
 
-export default function Todo() {
-  const [state, setState] = useState({
-    todoList: [],
-    input: '',
-    id: 0,
-  });
-  const { todoList, input } = state;
-
-  const handleChangeInput = ({ target: { value } }) => {
-    setState({
-      ...state,
-      input: value,
-    });
-  };
-
-  const handleClickAddTodo = () => {
-    if (input.trim() !== '') {
-      setState((prevState) => ({
-        ...state,
-        input: '',
-        id: prevState.id + 1,
-        todoList: [
-          ...todoList,
-          {
-            id: state.id,
-            text: input,
-          },
-        ],
-      }));
-    }
-  };
-
-  const handleClickRemoveTodo = (id) => {
-    setState({
-      ...state,
-      todoList: todoList.filter((item) => item.id !== id),
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    handleClickAddTodo();
-  };
-
+export default function Todo({
+  todoList,
+  input,
+  handleChangeInput,
+  handleClickAddTodo,
+  handleClickRemoveTodo,
+  handleSubmit,
+}) {
   return (
     <div>
       <h1>
