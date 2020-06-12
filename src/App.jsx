dbb9fd6fa18a43d45f5ba3d035668df977e2cae6
eonsang <2eonhworking@gmail.com>
2020-06-13 08:39:52 +0900

    state 합치기

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,41 @@ import React, { useState } from 'react';
 import Todo from './pages/Todo';
 
 export default function App() {
-  const [todoList, setTodoList] = useState([]);
-  const [input, setInput] = useState('');
-  const [nextId, setNextId] = useState(0);
+  const [state, setState] = useState({
+    todoList: [],
+    input: '',
+    nextId: 0,
+  });
+  const { todoList, input, nextId } = state;
 
   const handleChangeInput = ({ target: { value } }) => {
-    setInput(value);
+    setState({
+      ...state,
+      input: value,
+    });
   };
 
   const handleClickRemoveTodo = (id) => {
-    setTodoList(
-      todoList.filter((item) => item.id !== id),
-    );
+    setState({
+      ...state,
+      todoList: todoList.filter((item) => item.id !== id),
+    });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (input.trim() !== '') {
-      setTodoList([
-        ...todoList,
-        {
-          id: nextId,
-          text: input,
-        },
-      ]);
-      setInput('');
-      setNextId(nextId + 1);
+      setState({
+        todoList: [
+          ...todoList,
+          {
+            id: nextId,
+            text: input,
+          },
+        ],
+        input: '',
+        nextId: nextId + 1,
+      });
     }
   };
 
