2f383f66393d5664b0aa1da35bad272571e500c4
naraekn <34229459+naraekn@users.noreply.github.com>
2020-06-13 02:25:34 +0900

    Update todo app
    Submit assignment2-2 as the third try
    Modify conditional expression to check haveTodo

--- src/Page.jsx
+++ src/Page.jsx
@@ -5,13 +5,14 @@ import Todos from './Todos';
 export default function Page({ addFormState, todoState }) {
   const { newTodo, handleChange, handleSubmit } = addFormState;
   const { ids, todos, handleClickComplete } = todoState;
+  const haveToDo = (arr) => arr.length !== 0;
 
   return (
     <div>
       <h1>To-do</h1>
       <AddForm newTodo={newTodo} onChange={handleChange} onSubmit={handleSubmit} />
       {
-        todos.length
+        haveToDo(todos)
           ? <Todos ids={ids} todos={todos} onClick={handleClickComplete} />
           : <p>할 일이 없어요!</p>
       }
