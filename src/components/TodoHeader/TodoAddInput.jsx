9fa1273f7e81ce96108c399b1470d60a01387672
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-12 09:35:31 +0900

    refactor(TodoAddInput): onChange
    - onChange 핸들러에 불필요한 e 인자 전달 삭제

--- src/components/TodoHeader/TodoAddInput.jsx
+++ src/components/TodoHeader/TodoAddInput.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function TodoAddInput({ inputValue, onChange }) {
   return (
-    <input value={inputValue} onChange={(e) => onChange(e)} />
+    <input value={inputValue} onChange={onChange} />
   );
 }
