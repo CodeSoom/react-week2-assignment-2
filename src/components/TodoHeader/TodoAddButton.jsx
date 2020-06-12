922523718e364f6be2bce89df9fe9f8e2e42509f
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-12 09:28:12 +0900

    refactor(TodoAddButton)
    - 필요없는 함수 제거

--- src/components/TodoHeader/TodoAddButton.jsx
+++ src/components/TodoHeader/TodoAddButton.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
 export default function TodoAddButton({ onClick }) {
-  function handleClickAdd() {
-    onClick();
-  }
-
   return (
-    <button type="button" onClick={handleClickAdd}>추가</button>
+    <button type="button" onClick={onClick}>추가</button>
   );
 }
