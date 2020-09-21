ee59f5abb19d29b96ad7f37ec372b19f4f1c8a10
juunini <juuni.ni.i@gmail.com>
2020-09-21 23:13:41 +0900

    Make sample view

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,19 @@ import React from 'react';
 function App() {
   return (
     <div>
-      Hello, World!
+      <h1>To-do</h1>
+      <p>
+        <input
+          type="text"
+          placeholder="할 일을 입력해주세요"
+        />
+        <button
+          type="button"
+        >
+          추가
+        </button>
+      </p>
+      <p>할 일이 없어요!</p>
     </div>
   );
 }
