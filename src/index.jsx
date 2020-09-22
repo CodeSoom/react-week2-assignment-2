8101954dec7f52534984331711fc20d2f78d1bec
hyeonhyejin <dev.hyejin04@gmail.com>
2020-09-22 16:17:43 +0900

    Implement the function of the complete work button.

--- src/index.jsx
+++ src/index.jsx
@@ -2,38 +2,41 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 function App() {
-  const [todos, setTodoList] = useState([]);
+  const [works, setTodos] = useState([]);
 
-  const [todo, setTodo] = useState('');
+  const [work, setTodo] = useState('');
 
-  function handleTodoTextChange(event) {
+  function handleWorkTextChange(event) {
     setTodo(event.target.value);
   }
 
-  function handleAddTodoButtonClick() {
-    setTodoList([...todos, todo]);
+  function handleAddWorkButtonClick() {
+    setTodos([...works, { key: Date.now(), value: work }]);
     setTodo('');
+  }
+
+  function handleCompleteWorkButtonClick(deleteTodoKey) {
+    setTodos(works.filter(({ key }) => key !== deleteTodoKey));
   }
 
   return (
     <div>
       <h1>TO-DO</h1>
       <div>
-        <input type="text" placeholder="할 일을 입력해주세요." value={todo} onChange={(e) => handleTodoTextChange(e)} />
-        <button type="button" value="추가" onClick={handleAddTodoButtonClick}>추가</button>
+        <input type="text" placeholder="할 일을 입력해주세요." value={work} onChange={(e) => handleWorkTextChange(e)} />
+        <button type="button" value="추가" onClick={handleAddWorkButtonClick}>추가</button>
       </div>
 
       <div>
         {
-          todos.length || <p>할 일이 없어요!</p>
+          works.length <= 0 ? <p>할 일이 없어요!</p> : ''
         }
-
         <ol>
           {
-            todos.map((v, i) => (
-              <li>
-                {v}
-                <button type="button"> 완료</button>
+            works.map(({ key, value }) => (
+              <li key={key}>
+                {value}
+                <button type="button" onClick={() => handleCompleteWorkButtonClick(key)}> 완료</button>
               </li>
             ))
           }
