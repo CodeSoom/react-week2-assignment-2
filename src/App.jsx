646ec97ab97540562929bc4ea06964167906b0ed
gringrape <53764714+gringrape@users.noreply.github.com>
2020-09-22 22:09:25 +0900

    Add 'tasks' state and make submit handler

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,35 @@ import React, { useState } from 'react';
 export default function App() {
   const [state, setState] = useState({
     task: '',
+    tasks: [
+      { name: '할일 1', date: '2020-09-23' },
+    ],
   });
 
-  const { task } = state;
+  const { task, tasks } = state;
 
   const handleChange = (e) => {
     setState({
+      ...state,
       task: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setState({
+      task: '',
+      tasks: [...tasks, {
+        name: task,
+        date: new Date(),
+      }],
     });
   };
 
   return (
     <div>
       <h1>To-do</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="할 일을 입력해 주세요"
@@ -25,9 +40,16 @@ export default function App() {
         />
         <button type="submit">추가</button>
       </form>
-      <p>
-        {task}
-      </p>
+      {tasks
+        ? tasks.map(({ name, date }, index) => (
+          <p key={date}>
+            {index + 1}
+            .
+            {name}
+            <button type="button">완료</button>
+          </p>
+        ))
+        : '할 일이 없어요'}
     </div>
   );
 }
