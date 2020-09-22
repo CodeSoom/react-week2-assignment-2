a2d42407b1e9d2b138f0c9b0c396b8c498fb6695
davidyang2149 <davidyang2149@gmail.com>
2020-09-22 22:59:10 +0900

    Add make todoList
    2강 React - 2주 차 과제 2
    - index.jsx 기능 구현 완료.
    - (해야할 일) 리팩토링 진행.
    - (해야할 일) 컴포넌트 분리.

--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [todoInput, setTodoInput] = useState('');
+
+  const handleClickAddTodo = () => {
+    setTodoList([todoInput, ...todoList]);
+    setTodoInput('');
+  };
+
+  const handleChangeInput = (event) => {
+    setTodoInput(event.target.value);
+  };
+
+  const handleClickDone = (id) => {
+    setTodoList(todoList.filter((item) => item !== id));
+  };
+
   return (
     <div>
       <h1>To-do</h1>
-      <input type="text" />
-      <button type="button">추가</button>
-      <p>할 일이 없어요!</p>
-      <ol>
-        <li>
-          할 일
-          <button type="button">완료</button>
-        </li>
-        <li>
-          아무것도 하지 않기
-          <button type="button">완료</button>
-        </li>
-      </ol>
+      <input type="text" placeholder="할 일을 입력해 주세요" value={todoInput} onChange={handleChangeInput} />
+      <button type="button" onClick={handleClickAddTodo}>추가</button>
+      {
+        todoList.length === 0 ? (
+          <p>할 일이 없어요!</p>
+        ) : (
+            <ol>
+              {todoList.map((i) => (
+                <li key={i}>
+                  {i}
+                  <button type="button" onClick={() => handleClickDone(i)}>완료</button>
+                </li>
+              ))}
+            </ol>
+          )
+      }
     </div>
   );
 }
