2d97c747522a0408154a0828faa2976c1eb1c59c
hyeonhyejin <dev.hyejin04@gmail.com>
2020-09-22 13:54:36 +0900

    Implement the to-do app UI

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,27 @@ import ReactDOM from 'react-dom';
 function App() {
   return (
     <div>
-      hello
+      <h1>TO-DO</h1>
+      <div>
+        <input type="text" placeholder="할 일을 입력해주세요." />
+        <button type="button">추가</button>
+      </div>
+
+      <div>
+        <p>할 일이 없어요.</p>
+
+        <ol>
+          <li>
+            할일1
+            <button type="button">완료</button>
+          </li>
+          <li>
+            할일2
+            <button type="button">완료</button>
+          </li>
+
+        </ol>
+      </div>
     </div>
   );
 }
