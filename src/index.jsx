7514221ccf8f53b4fe6e122767411c866030a881
hyeonhyejin <dev.hyejin04@gmail.com>
2020-09-22 15:09:24 +0900

    Implement the add-todo function.

--- src/index.jsx
+++ src/index.jsx
@@ -1,28 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 function App() {
+  const [todos, setTodoList] = useState([]);
+
+  const [todo, setTodo] = useState('');
+
+  function handleTodoTextChange(event) {
+    setTodo(event.target.value);
+  }
+
+  function handleAddTodoButtonClick() {
+    setTodoList([...todos, todo]);
+    setTodo('');
+  }
+
   return (
     <div>
       <h1>TO-DO</h1>
       <div>
-        <input type="text" placeholder="할 일을 입력해주세요." />
-        <button type="button">추가</button>
+        <input type="text" placeholder="할 일을 입력해주세요." value={todo} onChange={(e) => handleTodoTextChange(e)} />
+        <button type="button" value="추가" onClick={handleAddTodoButtonClick}>추가</button>
       </div>
 
       <div>
-        <p>할 일이 없어요.</p>
+        {
+          todos.length || <p>할 일이 없어요!</p>
+        }
 
         <ol>
-          <li>
-            할일1
-            <button type="button">완료</button>
-          </li>
-          <li>
-            할일2
-            <button type="button">완료</button>
-          </li>
-
+          {
+            todos.map((v, i) => (
+              <li>
+                {v}
+                <button type="button"> 완료</button>
+              </li>
+            ))
+          }
         </ol>
       </div>
     </div>
