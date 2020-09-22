adf6030fc19519bc873947236ad7f4b083e83391
oceanide <oceanide@gmail.com>
2020-09-22 14:36:28 +0900

    Check if the array of TodoItems is empty

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -3,6 +3,14 @@ import React from 'react';
 import TodoItem from './TodoItem';
 
 export default function TodoList({ todoItems }) {
+  const isArrayEmpty = (arr) => (Array.isArray(arr) && arr.length === 0);
+
+  if (isArrayEmpty(todoItems)) {
+    return (
+      <div>할 일이 없어요!</div>
+    );
+  }
+
   return (
     <ul>
       {todoItems.map(({ id, todoText }, index) => (
