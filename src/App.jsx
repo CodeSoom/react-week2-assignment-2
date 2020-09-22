5723c1024a662de3450c18927695ee084c930162
gringrape <53764714+gringrape@users.noreply.github.com>
2020-09-22 21:36:29 +0900

    Add 'form' tag to take input

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ export default function App() {
   return (
     <div>
       <h1>To-do</h1>
+      <form>
+        <input type="text" placeholder="할 일을 입력해 주세요" />
+        <button type="submit">추가</button>
+      </form>
     </div>
   );
 }
