489c50d0c1361c7861722b99aecc6ecb4b88075f
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-09-22 14:11:58 +0900

    Display '할 일이 없어요!' in DOM
    Display '할 일이 없어요!' in DOM when there's nothing in todoItems.

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import TodoItem from './TodoItem';
 
 function TodoList({ todoItems, deleteTodoItem }) {
+  if (todoItems.length === 0) return <p>할 일이 없어요!</p>;
   return (
     <ul>
       {todoItems.map((item, index) => <TodoItem item={item} deleteTodoItem={deleteTodoItem} key={`todoItem${index}`} />)}
