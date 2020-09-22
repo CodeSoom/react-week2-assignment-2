baf9a4a28292bf283d974a97d237b1021476a1da
gringrape <53764714+gringrape@users.noreply.github.com>
2020-09-22 21:51:03 +0900

    Add state 'task' and take input as task

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [state, setState] = useState({
+    task: '',
+  });
+
+  const { task } = state;
+
+  const handleChange = (e) => {
+    setState({
+      task: e.target.value,
+    });
+  };
+
   return (
     <div>
       <h1>To-do</h1>
       <form>
-        <input type="text" placeholder="할 일을 입력해 주세요" />
+        <input
+          type="text"
+          placeholder="할 일을 입력해 주세요"
+          value={task}
+          onChange={handleChange}
+        />
         <button type="submit">추가</button>
       </form>
+      <p>
+        {task}
+      </p>
     </div>
   );
 }
