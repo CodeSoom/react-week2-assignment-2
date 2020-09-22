3709ba7ea91aa78cea0ca9905a96142e9e129b25
hyeonhyejin <dev.hyejin04@gmail.com>
2020-09-22 13:44:00 +0900

    Init the project
    - Add webpack-dev-server
    - 리액트를 사용하여 화면을 그리기 위한 기본 포맷을 작성했다.

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      hello
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app'),
+);
