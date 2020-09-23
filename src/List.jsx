c3ea4c30cd21f9f443e8101f2aa66a03dfb2dbe9
Noah <mike1124@naver.com>
2020-09-23 23:45:37 +0900

    ⚡️ Add list-style none

--- src/List.jsx
+++ src/List.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 function List({ onClick, toDos }) {
+  const listStyle = {
+    listStyle: 'none',
+  };
   return (
     <>
       {toDos.map(({ toDo, id }) => (
-        <li key={id}>
+        <li style={listStyle} key={id}>
           {toDo}
+          {' '}
           <button type="button" onClick={() => onClick(id)}>
             완료
           </button>
