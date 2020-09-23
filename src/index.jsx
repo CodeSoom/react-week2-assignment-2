1923c46cdece9ff3259efccf3c3fb3337e51a557
Dokyun Lim <im.koremp@gmail.com>
2020-09-23 17:04:46 +0900

    Update src/index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import TodoList from './TodoList';
+
+function App() {
+  const [todoList, setTodoList] = useState([]);
+
+  const [todo, setTodo] = useState('');
+
+  const [id, setId] = useState(1);
+
+  function onChange(e) {
+    setTodo(e.target.value);
+  }
+
+  function onCreate() {
+    if (todo === '') {
+      return;
+    }
+
+    setTodoList([...todoList, { id, todo }]);
+    setId(id + 1);
+    setTodo('');
+  }
+
+  function onRemove(key) {
+    setTodoList(todoList.filter((todoItem) => todoItem.id !== key));
+  }
+
+  return (
+    <div>
+      <h1>To-do</h1>
+      <div>
+        <input placeholder="할 일을 입력해 주세요" value={todo || ''} onChange={onChange} />
+        <button type="button" onClick={onCreate}>
+          추가
+        </button>
+        <TodoList todoList={todoList} onRemove={onRemove} />
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
