2f268146075a016abe41182dc11c94381a1c9b33
gringrape <53764714+gringrape@users.noreply.github.com>
2020-09-23 14:05:32 +0900

    Form 컴포넌트의 파라메터 이름 수정을 빼먹었습니다..

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-export default function Form({ inputTask, onSubmit, onChange }) {
+export default function Form({ newTask, onSubmit, onChange }) {
   return (
     <form onSubmit={onSubmit}>
       <input
         type="text"
         placeholder="할 일을 입력해 주세요"
-        value={inputTask}
+        value={newTask}
         onChange={onChange}
       />
       <button type="submit">추가</button>
