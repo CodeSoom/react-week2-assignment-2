2ba36d8c326271997913d082fa56a6e7620feba2
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-09-23 12:32:30 +0900

    Fix problems.
    Fix the eslint error and Make the Input onchange code short.

--- src/Input.jsx
+++ src/Input.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Input({ placeholder, value, onChange }) {
   return (
-    <input type="text" placeholder={placeholder} value={value} onChange={(e) => onChange(e)} />
+    <input type="text" placeholder={placeholder} value={value} onChange={onChange} />
   );
 }
 export default Input;
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 import InputBox from './InputBox';
 import TodoList from './TodoList';
 
-function Todo({ onClickAddButton, onInputChange, value, todoItems, removeSelectedTodo }) {
+function Todo({
+  onClickAddButton, onInputChange, value, todoItems, removeSelectedTodo,
+}) {
   return (
     <div>
       <h1>To-do</h1>
