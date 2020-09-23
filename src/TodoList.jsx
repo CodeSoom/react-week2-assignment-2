1bfa20afd97a7591f989e75348006bee9289a798
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-09-23 09:57:28 +0900

    Replace if statement to conditional operator.
    Replace if statement to conditional operator to simplify code.

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -3,16 +3,17 @@ import React from 'react';
 import TodoItem from './TodoItem';
 
 function TodoList({ todoItems, removeSelectedTodo }) {
-  if (todoItems.length === 0) return <p>할 일이 없어요!</p>;
   return (
     <ul>
-      {todoItems.map((item) => (
-        <TodoItem
-          item={item}
-          removeSelectedTodo={removeSelectedTodo}
-          key={item.id}
-        />
-      ))}
+      {todoItems.length === 0
+        ? <p>할 일이 없어요!</p>
+        : (todoItems.map((item) => (
+          <TodoItem
+            item={item}
+            removeSelectedTodo={removeSelectedTodo}
+            key={item.id}
+          />
+        )))}
     </ul>
   );
 }
