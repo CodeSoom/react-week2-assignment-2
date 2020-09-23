8fc574be837ccdf5d75b95884966a111abae062e
oceanide <oceanide@gmail.com>
2020-09-23 09:38:27 +0900

    Move content of handleClick() to onClick property of button

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 
 export default function TodoItem({ id, todoText, onClick }) {
-  const handleClick = () => {
-    onClick(id);
-  };
-
   return (
     <li>
       {todoText}
-      <button type="button" onClick={handleClick}>완료</button>
+      <button type="button" onClick={() => onClick(id)}>완료</button>
     </li>
   );
 }
