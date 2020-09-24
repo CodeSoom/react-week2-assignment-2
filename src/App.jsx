89a92e292b6e270d3a0a5fc77fecfca5dd19853b
saseungmin <dbd02169@naver.com>
2020-09-25 00:42:11 +0900

    Fixed Code Review
    - e는 UI 컴포넌트에서 관리
    - 함수명 변경

--- src/App.jsx
+++ src/App.jsx
@@ -6,34 +6,23 @@ import TodoTemplate from './components/TodoTemplate';
 
 function App() {
   const [todos, setTodos] = useState([]);
-  const [todo, setTodo] = useState('');
-
-  const handleOnChange = (e) => {
-    setTodo(e.target.value);
-  };
 
   const onInsert = (value) => {
-    const item = {
+    const todo = {
       id: todos.length ? todos[todos.length - 1].id + 1 : 1,
       value,
     };
 
-    setTodos([...todos, item]);
-  };
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    onInsert(todo);
-    setTodo('');
+    setTodos([...todos, todo]);
   };
 
   const handleRemoveTodo = (id) => {
-    setTodos(todos.filter((item) => item.id !== id));
+    setTodos(todos.filter((todo) => todo.id !== id));
   };
 
   return (
     <TodoTemplate>
-      <TodoInsert onChange={handleOnChange} onSubmit={onSubmit} todo={todo} />
+      <TodoInsert insert={onInsert} />
       <TodoList todos={todos} onRemove={handleRemoveTodo} />
     </TodoTemplate>
   );
--- src/components/TodoInsert.jsx
+++ src/components/TodoInsert.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function TodoInsert({ onChange, onSubmit, todo }) {
+function TodoInsert({ insert }) {
+  const [todo, setTodo] = useState('');
+
+  const handleOnChange = (e) => {
+    setTodo(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    insert(todo);
+    setTodo('');
+  };
+
   return (
-    <form onSubmit={onSubmit}>
-      <input placeholder="할 일을 입력하세요!" value={todo} onChange={onChange} />
+    <form onSubmit={handleSubmit}>
+      <input placeholder="할 일을 입력하세요!" value={todo} onChange={handleOnChange} />
       <button type="submit">추가</button>
     </form>
   );
