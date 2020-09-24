cb220470ec369e125f6964da8c6bfd57cd61bb5f
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-09-24 09:43:42 +0900

    Change the state structure.
    Combine todoItems and inputValue.

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,27 @@ import React, { useState } from 'react';
 import Todo from './Todo';
 
 function App() {
-  const [todoItems, setTodoItems] = useState([]);
-  const [inputValue, setInputValue] = useState('');
+  const [state, setState] = useState({
+    inputValue: '',
+    todoItems: [],
+
+  });
+
+  const { inputValue, todoItems } = state;
 
   function handleDoneButtonClick(selectedItem) {
     const newTodoItems = todoItems.filter((item) => item.id !== selectedItem.id);
-    setTodoItems(newTodoItems);
+    setState({ inputValue, todoItems: newTodoItems });
   }
 
   function handleInputSubmit(event) {
     event.preventDefault();
     if (!inputValue.trim()) return;
-    setTodoItems([...todoItems, { todo: inputValue, id: new Date() }]);
-    setInputValue('');
+    setState({ inputValue: '', todoItems: [...todoItems, { todo: inputValue, id: new Date() }] });
   }
 
   function handleInputChange(event) {
-    setInputValue(event.target.value);
+    setState({ inputValue: event.target.value, todoItems });
   }
 
   return (
