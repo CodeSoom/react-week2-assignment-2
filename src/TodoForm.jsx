2a58054922550c978364be4cb4649969182cf64a
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-09-24 10:00:36 +0900

    Replace Button component to submit button.

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import Button from './Button';
+
 import Input from './Input';
 
 function TodoForm({ value, onInputSubmit, onInputChange }) {
   return (
     <form onSubmit={onInputSubmit}>
       <Input placeholder="할 일을 입력해 주세요" value={value} onChange={onInputChange} />
-      <Button onClick={onInputSubmit}>
-        추가
-      </Button>
+      <button type="submit">추가</button>
     </form>
   );
 }
