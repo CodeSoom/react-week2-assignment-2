3fa300dc8d921a95b79939aa5b3e4436871efc5a
seungyup <shinyep@hanmail.net>
2020-09-25 21:22:03 +0900

    form 태그로 수정

--- src/TodoInput.jsx
+++ src/TodoInput.jsx
@@ -4,9 +4,9 @@ export default function TodoInput({
   value, onChange, onClick,
 }) {
   return (
-    <p>
+    <form onSubmit={onClick}>
       <input type="text" value={value} onChange={onChange} placeholder="할 일을 입력해 주세요" />
-      <button type="button" onClick={onClick}>추가</button>
-    </p>
+      <button type="submit">추가</button>
+    </form>
   );
 }
