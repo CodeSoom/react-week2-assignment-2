d83d8759833ae2fafa48c547383d29dc2a2073f1
Dokyun Lim <im.koremp@gmail.com>
2020-09-26 01:58:50 +0900

    InputForm 가독성 향상

--- src/InputForm.jsx
+++ src/InputForm.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 function InputForm({ todo, onChange, onCreate }) {
   return (
     <div>
-      <input placeholder="할 일을 입력해 주세요" value={todo || ''} onChange={onChange} />
+      <input
+        type="text"
+        placeholder="할 일을 입력해 주세요"
+        value={todo || ''}
+        onChange={onChange}
+      />
       <button type="button" onClick={onCreate}>
         추가
       </button>
