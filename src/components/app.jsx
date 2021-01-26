852f00134f0cfc02f6958473bba733d9b45f6d6f
EL <ickyicky@gmail.com>
2021-01-26 20:54:41 +0900

    FEAT: 화면 구성

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,7 +3,24 @@ import React from 'react';
 function app() {
   return (
     <div>
-      <h1>TO-do</h1>
+      <h1>To-do</h1>
+      <form>
+        <input
+          type="text"
+          placeholder="할 일을 입력해 주세요"
+        />
+        <button type="button">추가</button>
+      </form>
+      <ol>
+        <li>
+          <span>할일 1</span>
+          <button type="button">완료</button>
+        </li>
+        <li>
+          <span>할일 2</span>
+          <button type="button">완료</button>
+        </li>
+      </ol>
     </div>
   );
 }
