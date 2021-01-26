ddcba1532e5124120f3720a52fe18ed51fb0bdbf
shinhyogeun <tls1gy2rms3@naver.com>
2021-01-26 15:36:50 +0900

    feat : nothingToDo Component 제작

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,33 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+
 import TodoInput from './todoInput';
 import AddButton from './addButton';
+import NothingToDo from './nothingToDo';
 
 function App() {
   const [state, setState] = useState({
-    todo: [] || '할일이 없어요!',
+    thing: '',
   });
+
+  function setInput(e) {
+    setState({
+      thing: e.target.value,
+    });
+  }
+
+  function getInput() {
+    const { thing } = state;
+
+    console.log(thing);
+  }
 
   return (
     <div>
-      <h1>To-Do</h1>
-      <TodoInput />
-      <AddButton />
+      <h1>To-do</h1>
+      <TodoInput onChange={setInput} />
+      <AddButton onClick={getInput} />
+      <NothingToDo />
     </div>
   );
 }
--- src/todoInput.jsx
+++ src/todoInput.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-export default function TodoInput() {
+export default function TodoInput({ onChange }) {
   return (
-    <input placeholder="할 일을 입력해주세요." />
+    <input onChange={onChange} placeholder="할 일을 입력해주세요." />
   );
 }
