1734243e50a6c7dbd648b4c2de440f64e3ee0f2d
dodoongtak <themighty.tak@gmail.com>
2021-01-26 20:15:38 +0900

    Create Page Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import Page from './Page/Page';
+
 function App() {
   return (
-    <div />
+    <Page />
   );
 }
 
---
+++ src/Page/Page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Page() {
+  return (
+    <div>
+      <h1>To-do</h1>
+    </div>
+  );
+}
+
+export default Page;
