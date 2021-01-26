37ece68a4d91bf2a63ffa836aa3cc9df81b6960a
EL <ickyicky@gmail.com>
2021-01-26 21:11:59 +0900

    FEAT: 컴포넌트 구성

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,26 +1,52 @@
 import React from 'react';
 
+function AddForm() {
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="할 일을 입력해 주세요"
+      />
+      <button type="button">추가</button>
+    </form>
+  );
+}
+
+function TodoList() {
+  return (
+    <ol>
+      {[
+        { contents: '할일 1', id: 1 },
+        { contents: '할일 2', id: 2 },
+      ].map((todoItem) => (
+        <TodoItem
+          todoItem={todoItem}
+          key={todoItem.id}
+        />
+      ))}
+    </ol>
+  );
+}
+
+function TodoItem({ todoItem }) {
+  return (
+    <li>
+      <span>{ todoItem.contents }</span>
+      <button
+        type="button"
+      >
+        완료
+      </button>
+    </li>
+  )
+}
+
 function app() {
   return (
     <div>
       <h1>To-do</h1>
-      <form>
-        <input
-          type="text"
-          placeholder="할 일을 입력해 주세요"
-        />
-        <button type="button">추가</button>
-      </form>
-      <ol>
-        <li>
-          <span>할일 1</span>
-          <button type="button">완료</button>
-        </li>
-        <li>
-          <span>할일 2</span>
-          <button type="button">완료</button>
-        </li>
-      </ol>
+      <AddForm />
+      <TodoList />
     </div>
   );
 }
