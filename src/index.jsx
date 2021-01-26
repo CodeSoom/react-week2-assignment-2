11dda4124cb48010f48c4f3eff8a5bc181ea7bb4
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-01-26 23:55:54 +0900

    Set useState value
    todo들을 관리하기 위해 여러 값을 가진 객체를 가진 todo 배열을 가지고
    있는 객체인 todoList로 관리하는 것으로 접근했습니다.

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+const numbers = [1, 2, 3, 4, 5];
+const listItems = numbers.map((number) => <li>{number}</li>);
+
+function handleSubmit() {
+
+}
+function makeTodo() {
+  const [state, setState] = useState({
+    todo: [{
+      key: 0,
+      title: '',
+      completed: false,
+    }],
+  });
+  const { todoList } = state;
+}
 const element = (
-    <div>
-        <h1>To-do</h1>
-        <input type="text" placeholder="할 일을 입력해 주세요"></input>
-        <button>추가</button>
-    </div>
+  <div>
+    <h1>To-do</h1>
+    <input
+      type="text"
+      placeholder="할 일을 입력해 주세요"
+    />
+    <button
+      type="submit"
+      onClick={handleSubmit}
+    >
+      제출
+    </button>
+    <ul>
+      {listItems}
+    </ul>
+  </div>
 );
 
-
 ReactDOM.render(
-    element,
-    document.getElementById('app'),
-  );
-  
+  element,
+  document.getElementById('app'),
+);
