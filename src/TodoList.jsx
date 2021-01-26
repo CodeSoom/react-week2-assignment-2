ffcecb721e244032cfa8ba8e883d49770ca582c2
Yoon <yoonhe@gmail.com>
2021-01-26 21:04:10 +0900

    Create TodoList & TodoListItem Component

---
+++ src/TodoList.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import TodoListItem from './TodoListItem';
+
+function TodoList({ todos, handleDeleteTodo }) {
+  if (todos.length === 0) {
+    return <p>할 일이 없어요!</p>;
+  }
+
+  return (
+    <ol>
+      {todos.map((todo) => (
+        <TodoListItem
+          key={todo.id}
+          todo={todo}
+          handleDeleteTodo={handleDeleteTodo}
+        />
+      ))}
+    </ol>
+  );
+}
+
+export default TodoList;
---
+++ src/TodoListItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TodoListItem({ todo, handleDeleteTodo }) {
+  return (
+    <li>
+      <span>{todo.content}</span>
+      <button type="button" onClick={() => handleDeleteTodo(todo.id)}>완료</button>
+    </li>
+  );
+}
+
+export default TodoListItem;
