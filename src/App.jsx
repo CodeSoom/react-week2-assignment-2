22a36990ac5350dbf6a961a2ff3fef69a30fa846
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-01-26 18:03:35 +0900

    Working: App.jsx 생성
    - TodolistPage 컴포넌트를 출력

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import TodoListPage from './TodoListPage';
+
+function App() {
+  return (
+    <div className="wrap">
+      <h1 className="title">To-do</h1>
+      <br />
+      <TodoListPage />
+    </div>
+  );
+}
+
+export default App;
