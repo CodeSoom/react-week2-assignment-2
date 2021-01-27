4f33ce8c1c9f62324a72f152031dcb95c59ba15a
datalater <the7mincheol@gmail.com>
2021-01-27 16:42:09 +0900

    Remove unnecessary fragment

--- src/TodoInsert.jsx
+++ src/TodoInsert.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 
 export default function TodoInsert({ userInput, onChange, onSubmit }) {
   return (
-    <>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          placeholder="할 일을 입력해주세요."
-          value={userInput}
-          onChange={onChange}
-        />
-        <button type="submit">추가</button>
-      </form>
-    </>
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        placeholder="할 일을 입력해주세요."
+        value={userInput}
+        onChange={onChange}
+      />
+      <button type="submit">추가</button>
+    </form>
   );
 }
