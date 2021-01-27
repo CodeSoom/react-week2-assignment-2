3ce101a4b49408203216749847c76ee43e982a94
dodoongtak <themighty.tak@gmail.com>
2021-01-27 11:46:55 +0900

    Update #2 - Add placeholder on input element
    For user friendly UI, placeholder is recommended to be added.

--- src/commons/Input.jsx
+++ src/commons/Input.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Input({ value, onChange }) {
   return (
-    <input type="text" value={value} onChange={onChange} />
+    <input type="text" placeholder="할 일을 입력해 주세요" value={value} onChange={onChange} />
   );
 }
 
