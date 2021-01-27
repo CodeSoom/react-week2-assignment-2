39658731db32ca140ad05851451f654eb4a23ec9
EL <ickyicky@gmail.com>
2021-01-27 22:39:09 +0900

    FEAT: 리스트 비어있을 때 할일이 없어요! 문구 출력

--- src/components/todoList.jsx
+++ src/components/todoList.jsx
@@ -5,13 +5,14 @@ import TodoItem from './todoItem';
 export default function TodoList({ todoList, handleDelete }) {
   return (
     <ol>
-      {todoList.map((todoItem) => (
+      {!!todoList.length && todoList.map((todoItem) => (
         <TodoItem
           key={todoItem.id}
           todoItem={todoItem}
           handleDelete={handleDelete}
         />
       ))}
+      {!todoList.length && <p>할 일이 없어요!</p>}
     </ol>
   );
 }
