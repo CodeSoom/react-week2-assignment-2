80b6383e52478a3cb401ae505b0bb6c627eae92f
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-01-28 12:09:14 +0900

    Fix to print many todo things
    변수에 저장된 여러 todo들을출력하도록 개선하였습니다.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 const storage = {
   todos: [
     { key: 1, title: 'title1', completed: false },
+    { key: 2, title: 'title2', completed: false },
+    { key: 3, title: 'title3', completed: false },
+    { key: 4, title: 'title4', completed: false },
   ],
 };
 
+function createLi(todo) {
+  const li = document.createElement('li');
+  const button = document.createElement('Button');
+
+  li.textContent = todo.title;
+
+  button.textContent = '완료';
+  li.appendChild(button);
+  return li;
+}
+
 const ol = document.getElementById('todo-list');
-const li = document.createElement('li');
-const button = document.createElement('Button');
-
-li.textContent = storage.todos[0].title;
-
-button.textContent = '완료';
-
-li.appendChild(button);
-ol.appendChild(li);
+storage.todos.map((todo) => ol.appendChild(createLi(todo)));
