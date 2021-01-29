e7b7a0161e5f8d650cf03bb058f72c153a033936
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-01-30 05:02:00 +0900

    Move components to folder 'components' in 'src' from 'src'
    프로젝트를 확장할때, 다른 속성의 파일들이 많아질 경우를 고려하여 컴퍼넌트들을 'components'파일로 따로 분류하였습니다.

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import App from './components/App';
 
 ReactDOM.render(
   <App />,
