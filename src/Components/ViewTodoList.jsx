0be59efde01480f1a6c446d079c088300f001a04
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-01-29 17:02:53 +0900

    변수명 변경: listItems -> todoList

--- src/Components/ViewTodoList.jsx
+++ src/Components/ViewTodoList.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 import Item from './Item';
 
 function ViewTodoList({ todos, deleteButton }) {
-  const listItems = todos.map(
+  const todoList = todos.map(
     (todo) => <Item key={todo} value={todo} deleteButton={deleteButton} />,
   );
 
-  return (todos.length ? (<ol>{listItems}</ol>) : (<p>할 일이 없어요!</p>));
+  return (todos.length ? (<ol>{todoList}</ol>) : (<p>할 일이 없어요!</p>));
 }
 export default ViewTodoList;
