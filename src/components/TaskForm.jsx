ee709b7df1d2f359429b5ea59dcaf13e631f71a4
dodoongtak <themighty.tak@gmail.com>
2021-01-29 12:16:08 +0900

    Refactor #3 - Fix typo 'placeholder'

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function TaskForm({ taskInput, onTaskInputChange, onTaskFormSubmit }) {
   return (
     <form onSubmit={onTaskFormSubmit}>
-      <input value={taskInput} placholder="할 일을 입력해 주세요" onChange={onTaskInputChange} />
+      <input value={taskInput} placeholder="할 일을 입력해 주세요" onChange={onTaskInputChange} />
       <button type="submit">추가</button>
     </form>
   );
