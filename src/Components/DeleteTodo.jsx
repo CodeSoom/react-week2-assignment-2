f5b91fe570b8218b05b20608a4866df9c013fbb6
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-01-29 16:33:02 +0900

    DeleteTodo props의 이름 변경: todo -> value

--- src/Components/DeleteTodo.jsx
+++ src/Components/DeleteTodo.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 function DeleteTodo({
-  todo, deleteButton,
+  value, deleteButton,
 }) {
   return (
     <li>
-      {todo}
-      <button type="button" name={todo} onClick={deleteButton}>완료</button>
+      {value}
+      <button type="button" name={value} onClick={deleteButton}>완료</button>
     </li>
   );
 }
--- src/Components/ViewTodoList.jsx
+++ src/Components/ViewTodoList.jsx
@@ -4,7 +4,7 @@ import DeleteTodo from './DeleteTodo';
 
 function ViewTodoList({ todoList, deleteButton }) {
   const listItems = todoList.map(
-    (todo) => <DeleteTodo key={todo} todo={todo} deleteButton={deleteButton} />,
+    (todo) => <DeleteTodo key={todo} value={todo} deleteButton={deleteButton} />,
   );
 
   return (todoList.length ? (<ol>{listItems}</ol>) : (<p>할 일이 없어요!</p>));
