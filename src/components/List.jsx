46740604cf2dc92295be27688b65273a1d84155c
daniel <daniel@innosonian.com>
2021-01-30 20:22:29 +0900

    할 일이 없을 때 표시하는 코드에 Guard Clauses 적용하기

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -3,11 +3,21 @@ import React from 'react';
 import Item from './Item';
 
 export default function List({ tasks, onClickDelete }) {
-    return (
-        <ol>
-            {tasks.map((task) => (
-                <Item key={task.id} task={task} onClickDelete={onClickDelete} />
-            ))}
-        </ol>
-    );
+    if (tasks.length == 0) {
+        return (
+            <p>
+                할 일이 없어요!
+            </p>
+        );
+    }
+    
+    if (tasks.length > 0) {
+        return (
+            <ol>
+                {tasks.map((task) => (
+                    <Item key={task.id} task={task} onClickDelete={onClickDelete} />
+                ))}
+            </ol>
+        );
+    }
 }
