377a5729f037afce36b225b75a1ae830f00b7a5a
gimsesu <tkfrn4799@gmail.com>
2021-01-31 14:51:16 +0900

    Create button '완료'
    - task를 입력하고 '추가' 버튼을 누르면 getElementById를 이용해
    input 창의 값이 초기화 되도록 했습니다.
    - '완료' 버튼을 누르면 할 일이 지워지도록 onClick을 구현했습니다.
    - 할 일이 없을 경우, '할 일이 없어요!'를 출력하도록 했습니다.

--- src/index.jsx
+++ src/index.jsx
@@ -5,41 +5,73 @@ function Title() {
   return <h1>To-do</h1>;
 }
 
-function Form({ onChange, onSubmit }) {
+function Default() {
+  return <p>할 일이 없어요!</p>;
+}
+
+function Input({ onChange, onSubmit }) {
   return (
     <form onSubmit={onSubmit}>
-      <input type="text" placeholder="할 일을 입력해 주세요" onChange={onChange} />
+      <input id="todoinput" type="text" placeholder="할 일을 입력해 주세요" onChange={onChange} />
       <input type="submit" value="추가" />
     </form>
   );
 }
 
-function List({ values }) {
+function List({ values, onClick }) {
   return (
-    values.map((a, i) => (
-      <div key={i.toString()}>
-        {a.value}
-      </div>
-    )));
+    <ol>
+      {
+        values.map((a, i) => (
+          <Task key={i.toString()} dataKey={i} value={a.value} onClick={onClick} />
+        ))
+      }
+    </ol>
+  );
+}
+
+function Task({ dataKey, value, onClick }) {
+  return (
+    <li>
+      {value}
+      <ButtonDone onClick={() => onClick(dataKey)} />
+    </li>
+  );
+}
+
+function ButtonDone({ onClick }) {
+  return <button type="button" onClick={onClick}>완료</button>;
 }
 
 function App() {
-  const [todo, setTodo] = useState({ value: '' });
-  const [todos, setTodos] = useState([]);
+  const [task, setTodo] = useState({ value: '' });
   function handleChange(event) {
     setTodo({ value: event.target.value });
   }
 
+  const [todoList, setTodoList] = useState([]);
   function handleSubmit(event) {
     event.preventDefault();
-    setTodos([...todos, { value: todo.value }]);
+    if (task.value === '') return;
+
+    setTodoList([...todoList, { value: task.value }]);
+    setTodo({ value: '' });
+    document.getElementById('todoinput').value = '';
+  }
+
+  function handleClickDone(index) {
+    return setTodoList(
+      [...todoList.slice(0, index), ...todoList.slice(index + 1, todoList.length)],
+    );
   }
 
   return (
     <div>
       <Title />
-      <Form onChange={handleChange} onSubmit={handleSubmit} />
-      <List values={[...todos]} />
+      <Input onChange={handleChange} onSubmit={handleSubmit} />
+      {todoList.length === 0
+        ? (<Default />)
+        : (<List values={[...todoList]} onClick={handleClickDone} />)}
     </div>
   );
 }
