95ca473283ba9cb1035bed9b1aeffc1a19cf7d93
최한울 <hanul.choi95@nts-corp.com>
2021-05-10 23:44:50 +0900

    [2주차] project init
    - 리액트 작업을 하기 전 간단한 세팅

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      <p>To-do</p>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
