6a082bd8f8316070321da2c5f4c03001fced1d5f
yujonglee <yujonglee@g.skku.edu>
2021-05-10 14:25:00 +0900

    isEmpty를 도입하라

--- src/List.jsx
+++ src/List.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import * as R from 'ramda';
 import Item from './Item';
 
 export default function List({ items, onClick }) {
-  const element = (items.length)
-    ? (
+  const element = (R.isEmpty(items))
+    ? <p>할 일이 없어요!</p>
+    : (
       <ol>
         {items.map((item) => (
           <Item
@@ -12,8 +14,7 @@ export default function List({ items, onClick }) {
           />
         ))}
       </ol>
-    )
-    : <p>할 일이 없어요!</p>;
+    );
 
   return element;
 }
