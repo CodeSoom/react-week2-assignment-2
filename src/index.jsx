2a90a90a824fac36bccd5cd8c235231f7cad8bdc
최한울 <hanul.choi95@nts-corp.com>
2021-05-11 00:04:35 +0900

    [2주차] 클릭 시 할일 추가 이벤트 구현
    - 클릭하면 임의의 할일이 추가됨

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 function App() {
+  const [state, setState] = useState({
+    todoList: [],
+  });
+
+  const { todoList } = state;
+
+  function handleClick() {
+    setState({
+      todoList: [...todoList, <li>할일</li>],
+    });
+  }
+
   return (
     <div>
       <p>To-do</p>
+      <input
+        type="text"
+        placeholder="할 일을 입력해 주세요"
+      />
+      <button type="button" onClick={handleClick}>추가</button>
+      <ol>
+        {todoList}
+      </ol>
     </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
+
+
