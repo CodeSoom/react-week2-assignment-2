3aa2112c8444bc2197ec1715fee934212b975fca
yujonglee <yujonglee@g.skku.edu>
2021-05-10 11:34:36 +0900

    ul태그를 ol태그로 교체하라

--- src/List.jsx
+++ src/List.jsx
@@ -4,14 +4,14 @@ import Item from './Item';
 export default function List({ items, onClick }) {
   const element = (items.length)
     ? (
-      <ul>
+      <ol>
         {items.map((item) => (
           <Item
             content={item.content}
             onClick={() => onClick(item.id)}
           />
         ))}
-      </ul>
+      </ol>
     )
     : <p>할 일이 없어요!</p>;
 
