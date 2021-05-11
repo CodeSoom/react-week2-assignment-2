a2813cbdb74bc58d507095edfc94870dfb50f177
yujonglee <yujonglee@g.skku.edu>
2021-05-11 09:42:55 +0900

    List.jsx에서 Guard Clauses를 사용하고 PropType를 추가하라

--- src/List.jsx
+++ src/List.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import * as R from 'ramda';
+import PropTypes from 'prop-types';
 
 import Item from './Item';
 
 export default function List({ items, onClick }) {
-  const element = (R.isEmpty(items))
-    ? <p>할 일이 없어요!</p>
-    : (
-      <ol>
-        {items.map((item) => (
-          <Item
-            content={item.content}
-            onClick={() => onClick(item.id)}
-          />
-        ))}
-      </ol>
-    );
+  if (R.isEmpty(items)) {
+    return <p>할 일이 없어요!</p>;
+  }
 
-  return element;
+  return (
+    <ol>
+      {items.map((item) => (
+        <Item
+          content={item.content}
+          onClick={() => onClick(item.id)}
+        />
+      ))}
+    </ol>
+  );
 }
+
+List.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.object).isRequired,
+  onClick: PropTypes.func.isRequired,
+};
