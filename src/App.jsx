51c314406294cf8e45a8e28c718a001bd46b69cb
seong-dodo <dyrnxm52@hanmail.net>
2021-05-11 12:50:27 +0900

    To-do 앱 구조만들기
    - 리액트 환경 설치
    - index.jsx 와 App.jsx로 분리
    - To-do 앱 구조 만들기

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <p>To-do</p>
+      <p>
+        <input type='text' placeholder='할 일을 입력해 주세요' ></input>
+        <button>추가</button>
+      </p>
+    </div>
+   );
+}
+
+export default App;
