fd8cd711a64198ef1360c7c3db2665f4115737b8
seong-dodo <dyrnxm52@hanmail.net>
2021-05-11 14:06:02 +0900

    To-do 앱 리액트로 완성
    - 상태, 동작, UI 완성

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [description, setDescription] = useState('');
+  const [currentId, setCurrentId] = useState(1);
+  const [todoList, setTodoList] = useState([]);
+
+  const onAdd = () => {
+    const todo = { id: currentId, description };
+    setCurrentId(currentId + 1);
+    setTodoList([...todoList, todo]);
+  }
+
+  const onDelete = (e) => {
+    const id = Number(e.target.dataset.id);
+    const newTodoList = todoList.filter(todo => todo.id !== id);
+    setTodoList(newTodoList);
+  }
+
   return (
     <div>
       <p>To-do</p>
       <p>
-        <input type='text' placeholder='할 일을 입력해 주세요' ></input>
-        <button>추가</button>
+        <input
+          type='text'
+          placeholder='할 일을 입력해 주세요'
+          value={description}
+          onChange={(e) => setDescription(e.target.value)} >
+        </input>
+        <button
+          type='button'
+          onClick={onAdd}>
+          추가
+        </button>
+      </p>
+      <p>
+        <ul>
+          {
+            todoList.map((todo) => (
+              <li key={todo.id} type='1'>
+                <span>{todo.description}</span>
+                <button data-id={todo.id} onClick={onDelete}>완료</button>
+              </li>
+            ))
+          }
+        </ul>
       </p>
     </div>
-   );
+  );
 }
 
 export default App;
