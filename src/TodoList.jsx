fa6a8af2f618c0b53d01214d2f2f0020d3dde4a1
최한울 <hanul.choi95@nts-corp.com>
2021-05-12 20:47:50 +0900

    [2주차] Guard Clauses 사용하여 조기 리턴하라
    - 복잡한 중첩 로직을 피하기 위해 가능한 한 빨리 반환하는 패턴 사용
    - 이를 통해 코드를 읽기 쉽고 이해하기 쉽게 유지

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -4,19 +4,17 @@ import PropTypes from 'prop-types';
 import Todo from './Todo';
 
 function TodoList({ todoList, onClickRemove }) {
+  if (todoList.length === 0) {
+    return <p>할 일이 없어요!</p>;
+  }
+
   return (
     <div>
-      {(todoList.length > 0 && (
-        <ol>
-          {todoList.map((todo) => (
-            <Todo key={todo} todo={todo} onClickRemove={onClickRemove} />
-          ))}
-        </ol>
-      )) || (
-        <p>
-          할 일이 없어요!
-        </p>
-      )}
+      <ol>
+        {todoList.map((todo) => (
+          <Todo key={todo} todo={todo} onClickRemove={onClickRemove} />
+        ))}
+      </ol>
     </div>
   );
 }
