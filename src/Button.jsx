3d8f4e2dc9810120653b302a49d6f08f5333d25f
dngwoodo <dngwoodo@gmail.com>
2021-05-14 08:18:52 +0900

    fix: Remove eslint rule

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const Button = ({ children, onClick }) => (
-  // eslint-disable-next-line react/jsx-props-no-spreading
   <button type="button" onClick={onClick}>{children}</button>
 );
 
