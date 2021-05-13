bc81d05a8484ada2d989e9e0af61d8ea044c8e30
dngwoodo <dngwoodo@gmail.com>
2021-05-13 23:38:08 +0900

    refactor: Add List component

---
+++ src/List.jsx
@@ -0,0 +1,18 @@
+import React, { Children, isValidElement, cloneElement } from 'react';
+
+const List = ({ tagName, children }) => {
+  const CustomTag = tagName;
+
+  return (
+    <CustomTag>
+      {
+        Children.map(children, (child) => {
+          if (isValidElement(child)) return cloneElement(child);
+          return child;
+        })
+      }
+    </CustomTag>
+  );
+};
+
+export default List;
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import EmptyTodoList from './EmptyTodoList';
+import List from './List';
 import TodoItem from './TodoItem';
 
 const TodoList = ({ todos, deleteTodo, checkTodo }) => {
   if (todos.length === 0) return <EmptyTodoList>할 일이 없어요!</EmptyTodoList>;
   return (
-    <ol>
+    <List tagName="ol">
       {todos.map((todo) => (
         <TodoItem
           key={todo.id}
@@ -14,7 +15,7 @@ const TodoList = ({ todos, deleteTodo, checkTodo }) => {
           checkTodo={checkTodo}
         />
       ))}
-    </ol>
+    </List>
   );
 };
 
