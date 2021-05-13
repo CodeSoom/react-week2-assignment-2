9ce803e98225171bd690c18fe8a7508796850dd8
dngwoodo <dngwoodo@gmail.com>
2021-05-14 08:38:05 +0900

    fix: Remove EmptyTodoList component

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import EmptyTodoList from './EmptyTodoList';
 import List from './List';
 import TodoItem from './TodoItem';
 
 export default function TodoList({ todos, deleteTodo, checkTodo }) {
-  if (todos.length === 0) return <EmptyTodoList>할 일이 없어요!</EmptyTodoList>;
+  if (todos.length === 0) return <p>할 일이 없어요!</p>;
   return (
     <List tagName="ol">
       {todos.map((todo) => (
