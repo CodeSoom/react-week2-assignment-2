f412dca48f04766485e0443710923af38ac2c2a2
dngwoodo <dngwoodo@gmail.com>
2021-05-13 09:52:48 +0900

    env: Setup render

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+  <div>
+    App
+  </div>
+);
+
+export default App;
+
+ReactDOM.render(<App />, document.querySelector('#app'));
