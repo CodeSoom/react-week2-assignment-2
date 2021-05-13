112dcb756c74ecea6ee8adb482ef70e27ce7eade
dngwoodo <dngwoodo@gmail.com>
2021-05-13 10:16:51 +0900

    feat: Add feature that add todo list

--- src/index.jsx
+++ src/index.jsx
@@ -3,15 +3,27 @@ import ReactDOM from 'react-dom';
 
 const App = () => {
   const [todos, setTodos] = useState([]);
+  const [value, setValue] = useState('');
+
+  const onChangeValue = (e) => {
+    setValue(e.target.value);
+  };
+
+  const addTodoList = () => {
+    setTodos([...todos, {
+      id: new Date(),
+      content: value,
+    }]);
+  };
 
   return (
     <div>
       <h2>Todo</h2>
-      <input type="text" />
-      <button type="button">추가</button>
+      <input type="text" onChange={onChangeValue} value={value} />
+      <button type="button" onClick={addTodoList}>추가</button>
       <ol>
         {todos.map((todo) => (
-          <li>
+          <li key={todo.id}>
             <p>{todo?.content}</p>
             <button type="button">완료</button>
           </li>
