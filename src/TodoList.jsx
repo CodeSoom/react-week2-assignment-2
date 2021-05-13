8f027b6ace3ca4a4765b7ff3d744335e2e64b350
dngwoodo <dngwoodo@gmail.com>
2021-05-13 14:27:54 +0900

    feat: Add feature that apply line-throuth when todo list is clicked

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './Button';
 
 const TodoList = ({ todo, deleteTodoList }) => {
   const onDeleteTodoList = (id) => () => deleteTodoList(id);
+  const [isTodoDone, setIsTodoDone] = useState(false);
 
   return (
-    <li>
+    <li
+      onClick={() => {
+        setIsTodoDone(!isTodoDone);
+      }}
+      className={isTodoDone ? 'done' : ''}
+      aria-hidden="true"
+    >
       <p>{todo?.content}</p>
       <Button type="button" onClick={onDeleteTodoList(todo.id)}>완료</Button>
     </li>
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
 import Page from './Page';
+import './style.css';
 
 const App = () => {
   const [todos, setTodos] = useState([]);
