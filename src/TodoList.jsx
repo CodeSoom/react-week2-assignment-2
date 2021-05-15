e6256db391880901e786373e0cc98085b086811c
dngwoodo <dngwoodo@gmail.com>
2021-05-15 19:46:22 +0900

    refactor: Add if block

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 import TodoItem from './TodoItem';
 
 export default function TodoList({ todos, onClickDeleteTodo, onClickCheckTodo }) {
-  if (todos.length === 0) return <p>할 일이 없어요!</p>;
+  if (todos.length === 0) {
+    return <p>할 일이 없어요!</p>;
+  }
+
   return (
     <ol>
       {todos.map((todo) => (
