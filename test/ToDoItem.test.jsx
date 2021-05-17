b82fe55ac2c4d44ace9e4413335b3f40667c04a2
studioLuda <studio.luda.work@gmail.com>
2021-05-17 15:47:08 +0900

    3주차 강의 듣고 따라하기. (개인 메모)

---
+++ test/ToDoItem.test.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+
+import ToDoItem from '../src/components/ToDoItem';
+
+// 테스트 코드를 작성하는 표현방식 키워드
+// describe - context - it
+// given - when- then
+test('renderToDoItem', () => {
+  const todo = {
+    id: 1,
+    title: '계획대로 공부하기',
+  };
+  const handleClick = jest.fn();
+  const { container, getByText } = render(
+    <ToDoItem todo={todo} onClick={handleClick} />,
+  );
+
+  expect(container).toHaveTextContent('계획대로 공부하기');
+  expect(container).toHaveTextContent('완료');
+
+  const button = getByText("완료");
+
+  expect(handleClick).not.toBeCalled();
+  //console.log(button);
+  fireEvent.click(button);
+
+  expect(handleClick).toBeCalledWith(1);
+});
