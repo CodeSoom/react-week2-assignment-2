b124c73075d52d0672a544694fa906d7e17e8748
psy082 <zhappy114@gmail.com>
2021-08-17 23:12:54 +0900

    feat: todo 작성 기능 구현
    - 메세지 입력 후 추가 버튼 클릭 시 할일 추가 기능 구현
    - 할일 항목 완료 버튼 클릭 시 할일 삭제 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const handleInput = ({ target }) => {
+    setTodo(target.value);
+  };
+
+  const handleAddButtonClick = () => {
+    setTodos([...todos, todo]);
+    setTodo('');
+  };
+
+  const handleCompleteButtonClick = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
     <div>
       <p>To-do</p>
+      <p>
+        <input type="text" value={todo} placeholder="할 일을 입력해 주세요" onChange={(event) => handleInput(event)} />
+        <button type="button" onClick={handleAddButtonClick}>추가</button>
+      </p>
+      <p>
+        {todos.length > 0
+          ? todos.map((_todo, index) => (
+            <div key={_todo}>
+              {`${index + 1}. ${_todo}`}
+              <button type="button" onClick={() => handleCompleteButtonClick(index)}>완료</button>
+            </div>
+          ))
+          : <div>할 일이 없어요!</div>}
+      </p>
     </div>
   );
 }
