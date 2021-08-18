a15944697f61bddd8294990df390579f92376847
passwd10 <parkinseo94@naver.com>
2021-08-18 15:40:46 +0900

    chore: prop-types 린트체크를 하지 않는다

--- .eslintrc.js
+++ .eslintrc.js
@@ -47,5 +47,6 @@ module.exports = {
     'arrow-spacing': ['error', { before: true, after: true }],
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
   },
 };
