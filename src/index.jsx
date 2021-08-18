dd15fd208ce7e2894743ebf38d7bab2da5798ecc
passwd10 <parkinseo94@naver.com>
2021-08-18 10:07:40 +0900

    feat: todo를 입력해 추가하는 기능을 구현한다

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,48 @@
+import { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 function App() {
+  const [todoContent, setTodoContent] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const handleOnChange = (event) => {
+    setTodoContent(event.target.value);
+  };
+
+  const handleClick = () => {
+    setTodos([...todos, {
+      id: todos.length,
+      content: todoContent,
+      isDone: false,
+    }]);
+  };
+
   return (
     <>
       <h1>To-do</h1>
       <input
         type="text"
         placeholder="할 일을 입력해 주세요"
+        onChange={handleOnChange}
       />
-      <button type="button">추가</button>
+      <button
+        type="button"
+        onClick={handleClick}
+      >
+        추가
+      </button>
+      <ol>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.content}
+            <button
+              type="button"
+            >
+              완료
+            </button>
+          </li>
+        ))}
+      </ol>
     </>
   );
 }
