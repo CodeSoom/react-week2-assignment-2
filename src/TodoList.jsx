a818e65f6b00a6d98834711a040eb95df6408931
psy082 <zhappy114@gmail.com>
2021-08-20 00:56:24 +0900

    refactor: TodoList component 삼항 연산자를 guard clauses로 변경

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,12 +1,14 @@
 export default function TodoList({ todos, onClick }) {
+  if (todos.length < 1) {
+    return <div>할 일이 없어요!</div>;
+  }
+
   return (
-    todos.length > 0
-      ? todos.map((_todo, index) => (
-        <div key={_todo}>
-          {`${index + 1}. ${_todo}`}
-          <button type="button" onClick={onClick}>완료</button>
-        </div>
-      ))
-      : <div>할 일이 없어요!</div>
+    todos.map((_todo, index) => (
+      <div key={_todo}>
+        {`${index + 1}. ${_todo}`}
+        <button type="button" onClick={onClick}>완료</button>
+      </div>
+    ))
   );
 }
