4044002ac40381ec9e07dcc986e417cc1cb19f47
Aiden <rheech22@gmail.com>
2021-08-19 15:06:02 +0900

    Refactor with guard clauses

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,15 +1,17 @@
 export default function List({ todos, onClick }) {
-  const Todo = todos.length
-    ? todos.map((todo) => (
-      <li key={todo.id}>
-        {todo.text}
-        <button type="button" onClick={() => onClick(todo.id)}>완료</button>
-      </li>
-    ))
-    : <span>할 일이 없어요!</span>;
+  if (!todos.length) {
+    return (
+      <p>할 일이 없어요!</p>
+    );
+  }
   return (
     <ol>
-      {Todo}
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          {todo.text}
+          <button type="button" onClick={() => onClick(todo.id)}>완료</button>
+        </li>
+      )) }
     </ol>
   );
 }
