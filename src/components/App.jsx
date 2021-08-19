c5cfea74dc9840449cc0c26638ad04c7f3eb79e7
Aiden <rheech22@gmail.com>
2021-08-19 14:56:42 +0900

    Add spread syntax more

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,30 +4,40 @@ import Form from './Form';
 import List from './List';
 
 export default function App() {
-  const [input, setInput] = useState('');
-  const [todos, setTodos] = useState([]);
+  const [state, setState] = useState({
+    input: '',
+    todos: [],
+  });
+
+  const { input, todos } = state;
 
   const todoID = useRef(0);
 
   function handleSubmit(e) {
     e.preventDefault();
-    setTodos(
-      [...todos,
+    setState({
+      input: '',
+      todos: [...todos,
         {
           id: todoID.current,
           text: input,
         }],
-    );
+    });
     todoID.current += 1;
-    setInput('');
   }
 
   function handleChange(e) {
-    setInput(e.target.value);
+    setState({
+      ...state,
+      input: e.target.value,
+    });
   }
 
   function handleDelete(id) {
-    setTodos(todos.filter((todo) => todo.id !== id));
+    setState({
+      ...state,
+      todos: todos.filter((todo) => todo.id !== id),
+    });
   }
 
   return (
