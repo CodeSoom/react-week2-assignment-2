89cc97aab58af2c0d2388fcae373b3ea2753b4c4
passwd10 <parkinseo94@naver.com>
2021-08-19 20:23:02 +0900

    refactor: TodoList에 guard clause를 적용한다

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,21 +3,23 @@ export default function TodoList({ todos, setTodos }) {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
 
+  if (!todos.length) {
+    return <p>할 일이 없어요!</p>;
+  }
+
   return (
     <ol>
-      {todos.length === 0
-        ? <p>할 일이 없어요!</p>
-        : todos.map(({ id, content }) => (
-          <li key={id}>
-            {content}
-            <button
-              type="button"
-              onClick={() => handleClickCompleteButton(id)}
-            >
-              완료
-            </button>
-          </li>
-        ))}
+      {todos.map(({ id, content }) => (
+        <li key={id}>
+          {content}
+          <button
+            type="button"
+            onClick={() => handleClickCompleteButton(id)}
+          >
+            완료
+          </button>
+        </li>
+      ))}
     </ol>
   );
 }
