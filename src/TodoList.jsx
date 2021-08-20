4229e26d013339f6a470469ea34d73a8bcf666c3
lazy-sky <1cielo@naver.com>
2021-08-20 15:01:22 +0900

    refact: Apply guard clause for making todoitem components

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -3,14 +3,17 @@ import React from 'react';
 import TodoItem from './TodoItem';
 
 export default function TodoList({ todoList, onClick }) {
+  if (!todoList.length) {
+    return (
+      <div>할 일이 없어요!</div>
+    );
+  }
   return (
     <ol>
       {
-        todoList.length > 0
-          ? todoList.map((todo) => (
-            <TodoItem key={todo.id} todo={todo} onClick={onClick} />
-          ))
-          : <div>할 일이 없어요!</div>
+        todoList.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} onClick={onClick} />
+        ))
       }
     </ol>
   );
