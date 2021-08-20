8af7de546e0184ccc3645f90d394fc9b4fc28d8b
hanabyhana <yaong1002@gmail.com>
2021-08-20 20:50:48 +0900

    Feat - ToDo App 기본 HTML 마크업, toDo 항목 완료 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
-import Page from './components/Page';
+import { useState } from 'react';
 
 export default function App() {
+  const [state, setState] = useState({
+    todos: [
+      { id: 0, todoName: '과제1' },
+      { id: 1, todoName: '과제2' },
+    ],
+  });
+
+  const { todos } = state;
+
+  function onClickCompleteTodo(completedTodo) {
+    const updatedTodos = todos.filter((todo) => todo.id !== completedTodo.id);
+    setState({
+      ...state,
+      todos: updatedTodos,
+    });
+  }
+
   return (
-    <Page />
+    <>
+      <h1>To-do</h1>
+      <form>
+        <input type="text" />
+        <button type="button">추가</button>
+      </form>
+      <ol>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.todoName}
+            <button type="button" onClick={() => onClickCompleteTodo(todo)}>완료</button>
+          </li>
+        ))}
+      </ol>
+    </>
   );
 }
--- src/components/Page.jsx
+++
@@ -1,5 +0,0 @@
-export default function Page() {
-  return (
-    <h1>To-do</h1>
-  );
-}
