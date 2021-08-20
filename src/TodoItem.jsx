322314e89c387fa58addd9d254ddddb465bebc0c
lazy-sky <1cielo@naver.com>
2021-08-20 14:23:11 +0900

    refact: Removing unused react imports

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export default function TodoItem({ key, todo, onClick }) {
   return (
     <li>
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import TodoItem from './TodoItem';
 
 export default function TodoList({ state, onClick }) {
