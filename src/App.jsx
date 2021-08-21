a712f4ecd9e009c00418bd5cb5d0b78379e60c6e
hanabyhana <yaong1002@gmail.com>
2021-08-21 20:04:43 +0900

    Feat - 입력 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import { useState } from 'react';
 export default function App() {
   const [state, setState] = useState({
     todos: [
-      { id: 0, todoName: '과제1' },
-      { id: 1, todoName: '과제2' },
+      // { id: 1, todoName: '과제1' },
+      // { id: 2, todoName: '과제2' },
     ],
+    newTodo: { id: 1, todoName: '' },
   });
 
-  const { todos } = state;
+  const { todos, newTodo } = state;
 
-  function onClickCompleteTodo(completedTodo) {
+  function handleClickCompleteTodo(completedTodo) {
     const updatedTodos = todos.filter((todo) => todo.id !== completedTodo.id);
     setState({
       ...state,
@@ -18,21 +19,43 @@ export default function App() {
     });
   }
 
+  function handleTypeNewTodo(event) {
+    setState({
+      ...state,
+      newTodo: { ...newTodo, todoName: event.target.value },
+    });
+  }
+
+  function handleClickAddTodo() {
+    setState({
+      ...state,
+      todos: [
+        ...todos,
+        newTodo,
+      ],
+      newTodo: { id: newTodo.id + 1, todoName: '' },
+    });
+  }
+
   return (
     <>
       <h1>To-do</h1>
       <form>
-        <input type="text" />
-        <button type="button">추가</button>
+        <input type="text" placeholder="할 일을 입력해 주세요" onChange={handleTypeNewTodo} value={newTodo.todoName} />
+        <button type="button" onClick={handleClickAddTodo}>추가</button>
       </form>
-      <ol>
-        {todos.map((todo) => (
-          <li key={todo.id}>
-            {todo.todoName}
-            <button type="button" onClick={() => onClickCompleteTodo(todo)}>완료</button>
-          </li>
-        ))}
-      </ol>
+      {todos.length !== 0
+        ? (
+          <ol>
+            {todos.map((todo) => (
+              <li key={todo.id}>
+                {todo.todoName}
+                <button type="button" onClick={() => handleClickCompleteTodo(todo)}>완료</button>
+              </li>
+            ))}
+          </ol>
+        )
+        : <p>할 일이 없어요!</p>}
     </>
   );
 }
