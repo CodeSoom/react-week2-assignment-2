7747ea4a021aff6cfc409a54fd9a967c20d37e55
developerOlive <chloehiho@gmail.com>
2021-08-22 21:34:41 +0900

    [삭제] 불필요한 주석 제거

--- src/List.jsx
+++ src/List.jsx
@@ -5,7 +5,7 @@ import Item from './Item';
 export default function List({ tasks, onClickDelete }) {
   return (
     <ol>
-      {tasks.map(task => ( // 배열의 요소를 하나씩 떼어네서 Item에 보낼 것. 
+      {tasks.map(task => (
         <Item 
           key={task.id}
           task={task} 
