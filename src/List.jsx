ac3aa18a57bda2b786923cbe841e5da1aca7d32a
developerOlive <chloehiho@gmail.com>
2021-08-22 20:56:39 +0900

    [수정] 가독성을 위한 리팩토링

--- src/List.jsx
+++ src/List.jsx
@@ -5,8 +5,12 @@ import Item from './Item';
 export default function List({ tasks, onClickDelete }) {
   return (
     <ol>
-      {tasks.map(task => (
-        <Item key={task.id} task={task} onClick={onClickDelete} />
+      {tasks.map(task => ( // 배열의 요소를 하나씩 떼어네서 Item에 보낼 것. 
+        <Item 
+          key={task.id}
+          task={task} 
+          onClick={onClickDelete} 
+        />
       ))}
     </ol>
   );
