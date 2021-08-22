c33602707f8d51089469d678438a72ab7de2e72a
hanabyhana <yaong1002@gmail.com>
2021-08-22 18:03:41 +0900

    Refactor - 조건부 렌더링 코드를 guard clauses 형태로 수정

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,15 +1,12 @@
 import TodoItem from './TodoItem';
 
 export default function TodoList({ todos, onClick }) {
+  if (todos.length === 0) {
+    return <p>할 일이 없어요!</p>;
+  }
   return (
-    <>
-      {(todos.length !== 0)
-        ? (
-          <ol>
-            {todos.map((todo) => <TodoItem key={todo.id} todo={todo} onClick={onClick} />)}
-          </ol>
-        )
-        : <p>할 일이 없어요!</p>}
-    </>
+    <ol>
+      {todos.map((todo) => <TodoItem key={todo.id} todo={todo} onClick={onClick} />)}
+    </ol>
   );
 }
