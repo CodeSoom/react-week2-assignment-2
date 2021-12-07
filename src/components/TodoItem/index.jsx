bf771d30831db08c2c752e40f86762520722564f
padawanR0k <padawanR0k@gmail.com>
2021-12-07 23:07:03 +0900

    new: add TodoItem

---
+++ src/components/TodoItem/index.jsx
@@ -0,0 +1,10 @@
+const TodoItem = ({ content, onClick }) => (
+  <div>
+    {content}
+    <button onClick={() => onClick()} type="button">
+      완료
+    </button>
+  </div>
+);
+
+export default TodoItem;
