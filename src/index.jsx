708dc12c46f7cc52b4fb62319b993d85aaadfcbd
jnny1017 <jnny1017@gmail.com>
2021-12-08 00:58:07 +0900

    Feat todo리스트 입력,추가,완료 기능 구현 todo를 입력, 추가, 완료하는 기능을 구현했습니다

--- src/index.jsx
+++ src/index.jsx
@@ -2,19 +2,57 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 function App() {
+  const [todo, setTodo] = useState('');
+  const [array, setArray] = useState([]);
+
+  function handleChange(event) {
+    const { target: { value } } = event;
+
+    setTodo(value);
+  }
+
+  function handleClickAdd() {
+    setArray([...array, todo]);
+    setTodo('');
+  }
+
+  function handleClickDone(item) {
+    setArray(array.filter((it) => it !== item));
+  }
+
   return (
     <>
       <h1>To-do</h1>
       <div>
-        <input type="text" placeholder="할 일을 입력해주세요." />
-        <button type="button">
-          추가
-        </button>
+        <form>
+          <input
+            type="text"
+            placeholder="할 일을 입력해주세요."
+            value={todo}
+            onChange={handleChange}
+          />
+          <button type="button" onClick={handleClickAdd}>
+            추가
+          </button>
+        </form>
       </div>
-      <p>할 일이 없어요!</p>
-      <ol>
-        <li></li>
-      </ol>
+      {array.length === 0
+        ? <p>할 일이 없어요!</p>
+        : (
+          <ol>
+            {array.map((item) => (
+              <li key={item}>
+                {item}
+                <button
+                  type="button"
+                  onClick={() => handleClickDone(item)}
+                >
+                  완료
+                </button>
+              </li>
+            ))}
+          </ol>
+        )}
     </>
   );
 }
