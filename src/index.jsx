aa464b8be4110af7262ccfc54ad2c6a24d2b515a
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-07 18:49:53 +0900

    사용할 컴포넌트 미리 구성하기

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,43 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-function App() {
+
+function Habit() {
+  const content = "none";
   return (
     <div>
-      App
+      {content}
+      <button type="button"> {content}</button>
     </div>
+  )
+}
 
+function Habits() {
+
+  return (
+    <div>habits</div>
+  )
+}
+
+function Input() {
+
+  return (
+    <form>
+      <input type="text" placeholder="할 일을 입력해 주세요" />
+      <input type="button" value="추가" id="" />
+    </form>
+  )
+}
+
+function Page() {
+  return (
+    <Input />
+  )
+}
+
+function App() {
+  return (
+    <Page />
   )
 }
 
