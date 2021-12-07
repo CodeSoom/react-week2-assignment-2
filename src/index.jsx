02dace5131e68b0f79c63e8ce1a8240bd63fe282
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-07 18:41:03 +0900

    test and setting

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+function App() {
+  return (
+    <div>
+      App
+    </div>
+
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
