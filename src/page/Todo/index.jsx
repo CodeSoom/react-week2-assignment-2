3a362968cde9a056cd166a13d31a40078e48794e
padawanR0k <padawanR0k@gmail.com>
2021-12-07 23:07:34 +0900

    new: add Todo Input

---
+++ src/page/Todo/index.jsx
@@ -0,0 +1,19 @@
+const ToDoInput = ({ addTodo }) => {
+  const [value, setValue] = React.useState();
+
+  return (
+    <div>
+      <input
+        onChange={({ target: { value } }) => setValue(value)}
+        value={value}
+        type="text"
+        placeholder="할 일을 입력해주세요"
+      />
+      <button onClick={() => addTodo(value)} type="button">
+        추가
+      </button>
+    </div>
+  );
+};
+
+export default ToDoInput;
