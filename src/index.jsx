1402f38a0e5a6cfd59981df5fd284f62d40d5174
padawanR0k <padawanR0k@gmail.com>
2021-12-07 23:52:01 +0900

    new:  add event callback in todo page

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import Todo from './page/Todo';
 
 const App = () => (
   <div>
-    To-Do
+    <Todo />
   </div>
 );
 
--- src/page/Todo/index.jsx
+++ src/page/Todo/index.jsx
@@ -1,19 +1,34 @@
-const ToDoInput = ({ addTodo }) => {
-  const [value, setValue] = React.useState();
+import { useState } from 'react';
+import ToDoList from '../../components/TodoList';
+import ToDoInput from '../../components/TodoInput';
+
+const ToDo = () => {
+  const [list, setList] = useState([]);
+
+  const addTodo = (content) => {
+    const maxId = list.length ? Math.max(...list.map(({ id }) => id)) : 0;
+    const newTodo = {
+      content,
+      id: maxId + 1,
+    };
+    setList([...list, newTodo]);
+  };
+
+  const completeTodo = (id) => {
+    const newList = list.filter((item) => item.id !== id);
+    setList(newList);
+  };
 
   return (
     <div>
-      <input
-        onChange={({ target: { value } }) => setValue(value)}
-        value={value}
-        type="text"
-        placeholder="할 일을 입력해주세요"
-      />
-      <button onClick={() => addTodo(value)} type="button">
-        추가
-      </button>
+      <ToDoInput addTodo={addTodo} />
+      {list.length ? (
+        <ToDoList list={list} completeTodo={completeTodo} />
+      ) : (
+        '할 일이 없어요!'
+      )}
     </div>
   );
 };
 
-export default ToDoInput;
+export default ToDo;
