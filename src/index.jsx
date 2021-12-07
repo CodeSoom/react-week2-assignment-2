018f5aef28ba810d8667c89a5c57f2d24a808ef4
CheolWoong Choi <bluesky4381@naver.com>
2021-12-07 23:08:16 +0900

    Implement todo and todos features

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 function App() {
+  const [newTodo, setNewTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const handleChangeTodo = (e) => {
+    setNewTodo(e.target.value);
+  };
+
+  const handleAddTodo = () => {
+    setNewTodo('');
+    setTodos([...todos, newTodo]);
+  };
+
   return (
     <div>
-      <h1>TODO</h1>
+      <h1>TO-DO</h1>
       <div>
-        <input type="text" placeholder="할 일을 입력해주세요!" />
-        <button type="button">추가</button>
+        <input
+          type="text"
+          placeholder="할 일을 입력해주세요!"
+          value={newTodo}
+          onChange={handleChangeTodo}
+        />
+        <button type="button" onClick={handleAddTodo}>추가</button>
       </div>
       <br />
-      <div>할 일이 없어요!</div>
+      {todos.length ? (
+        todos.map((todo) => (
+          <div>
+            <span>{todo}</span>
+            <span>{' '}</span>
+            <button type="button">확인</button>
+          </div>
+        ))
+      ) : (
+        <div>할 일이 없어요!</div>
+      )}
     </div>
   );
 }
