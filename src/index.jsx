b1a0e1b4cd31950c9c1b27b182be7833665f6c5f
shongs27 <shongs27@naver.com>
2021-12-08 23:34:08 +0900

    Feat - npm test 완료

--- src/index.jsx
+++ src/index.jsx
@@ -2,18 +2,20 @@ import { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 function TodoList({ texts, onClick }) {
+  if (!texts.length) return (<div>할 일이 없어요!</div>);
   return (
-    <ol>
-      {texts.map((text) => (
-        <div key={text}>
-          <li>
-            {text}
-            <button type="button" onClick={() => onClick(text)}>완료</button>
-          </li>
-        </div>
-      ))}
-    </ol>
-
+    <div>
+      <ol>
+        {texts.map((text) => (
+          <div key={text}>
+            <li>
+              {text}
+              <button type="button" onClick={() => onClick(text)}>완료</button>
+            </li>
+          </div>
+        ))}
+      </ol>
+    </div>
   );
 }
 
