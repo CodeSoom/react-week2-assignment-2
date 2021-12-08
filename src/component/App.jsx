d0ad15d84f8fa369b52942d53ed2b53cacd23bec
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-08 21:15:38 +0900

    [refactor] Page를 컴포넌트로 분리

---
+++ src/component/App.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+
+function App() {
+  const [state, setState] = useState({
+    habits: []
+  })
+  const { habits } = state;
+
+  function handleDelete(key) {
+    setState({
+      habits: habits.filter((value, index) => index != key)
+    }
+    )
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    setState({
+      habits: [e.target[0].value, ...habits]
+    })
+    e.target[0].value = "";
+  }
+
+
+  return (
+    <Page onSubmit={handleSubmit} onDelete={handleDelete} habits={habits} />
+  )
+}
+
+export default App
