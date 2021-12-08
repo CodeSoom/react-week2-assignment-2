0d01e7655bdfe93c7cf24f9f5dca92c9a071467f
shongs27 <shongs27@naver.com>
2021-12-08 21:56:22 +0900

    Feat - 항목제거 기능 구현

--- src/index.jsx
+++ src/index.jsx
@@ -1,62 +1,55 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+function TodoList({ texts, onClick }) {
+  return (
+    <ol>
+      {texts.map((text) => (
+        <div key={text}>
+          <li>
+            {text}
+            <button type="button" onClick={() => onClick(text)}>완료</button>
+          </li>
+        </div>
+      ))}
+    </ol>
 
-function TodoList({text, onClick}){
-  
-
-    return (
-        <ol>
-            {text.map((v) => (         
-                <div key={v} onClick={onClick}>       
-                    <li>{v}
-                    <button type="button" >완료</button>                    
-                    </li>
-                </div>
-            ))}
-        </ol>
-        
-    )
+  );
 }
 
-function TodoInput({text, onChange, onClick}) {
-    return (
-        <>
-        <div> 
-            <input type="text" placeholder="할 일을 입력해주세요" value={text} onChange={onChange}/>
-            <button type="button" onClick={onClick}>추가</button>
-        </div>            
-        </>
-    )
+function TodoInput({ text, onChange, onClick }) {
+  return (
+    <>
+      <div>
+        <input type="text" placeholder="할 일을 입력해주세요" value={text} onChange={onChange} />
+        <button type="button" onClick={onClick}>추가</button>
+      </div>
+    </>
+  );
 }
 
-function App(){
-    const [text, setText] = useState('')
-    function onChangeText(e){
-        setText(e.target.value);
-    }
+function App() {
+  const [text, setText] = useState('');
+  function onChangeText(e) {
+    setText(e.target.value);
+  }
 
-    const [submitText, setSubmitText] = useState([]);
-    function onClickSubmit(){
-        setSubmitText((prev) => ([...prev, text]))
-        setText('')        
-    }
+  const [submitText, setSubmitText] = useState([]);
+  function onClickSubmit() {
+    setSubmitText((prev) => ([...prev, text]));
+    setText('');
+  }
 
-    function onClickFinish(e) {
-        console.log(e.currentTarget.innerText)
-        setSubmitText((prev) => 
-             prev.filter((v) => {
-               return (v+'완료') !== e.currentTarget.innerText})
+  function onClickFinish(t) {
+    setSubmitText((prevTexts) => prevTexts.filter((prev) => prev !== t));
+  }
 
-        )
-    }
-
-    return (
-      <>
-        <TodoInput text={text} onChange ={onChangeText} onClick={onClickSubmit}/>
-        <TodoList text={submitText} onClick={onClickFinish} />
-      </>
-    )
+  return (
+    <>
+      <TodoInput text={text} onChange={onChangeText} onClick={onClickSubmit} />
+      <TodoList texts={submitText} onClick={onClickFinish} />
+    </>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
