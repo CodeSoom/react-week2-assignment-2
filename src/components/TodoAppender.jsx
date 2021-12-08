cbd0d622c16f0f29a732c29e51da5d925e17474f
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-12-08 20:14:57 +0900

    [reafactor] todoItem 추가 기능 수정 - DOM 제어 방식 대신 상태를 제어하는 방식으로 수정

--- src/components/TodoAppender.jsx
+++ src/components/TodoAppender.jsx
@@ -1,23 +1,26 @@
-import { useRef } from 'react';
+import { useState } from 'react';
 
 function TodoAppender({ onAddTodo }) {
-  const inputRef = useRef(null);
+  const [todoText, setTodoText] = useState('');
+
+  function handleChange({ target }) {
+    setTodoText(target.value);
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
 
-    const todoItemText = inputRef?.current.value;
-    if (!todoItemText) {
+    if (!todoText) {
       return;
     }
 
-    onAddTodo(todoItemText);
-    inputRef.current.value = '';
+    onAddTodo(todoText);
+    setTodoText('');
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" placeholder="할 일을 입력해 주세요" ref={inputRef} />
+      <input type="text" placeholder="할 일을 입력해 주세요" value={todoText} onChange={handleChange} />
       <button type="submit">추가</button>
     </form>
   );
