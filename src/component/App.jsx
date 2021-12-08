51bc13d1ca8a6bdab8ff3ae1d5362bd0c0788cb9
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-08 21:41:52 +0900

    [refactor] habit 데이터와 다루는 함수를 Page  컴포넌트에 옮김

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -3,31 +3,9 @@ import Page from './Page.jsx';
 
 
 function App() {
-  const [state, setState] = useState({
-
-    habits: []
-  })
-  const { habits } = state;
-
-  function handleDelete(key) {
-    setState({
-      habits: habits.filter((value, index) => index != key)
-    }
-    )
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault()
-    setState({
-      ...state,
-      habits: [e.target[0].value, ...habits]
-    })
-    e.target[0].value = "";
-  }
-
 
   return (
-    <Page onSubmit={handleSubmit} onClickDelete={handleDelete} habits={habits} />
+    <Page />
   )
 }
 
--- src/component/Page.jsx
+++ src/component/Page.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Input from './Input.jsx'
-function Page({ onSubmit, onClickDelete, habits }) {
+function Page() {
+
+  const [state, setState] = useState({
+    habits: []
+  })
+  const { habits } = state;
+
+  function handleDelete(key) {
+    setState({
+      habits: habits.filter((value, index) => index != key)
+    }
+    )
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    setState({
+      habits: [e.target[0].value, ...habits]
+    })
+    e.target[0].value = "";
+  }
+
   return (
-    <Input onSubmit={onSubmit} onClickDelete={onClickDelete} habits={habits} />
+    <Input onSubmit={handleSubmit} onClickDelete={handleDelete} habits={habits} />
   )
 }
 
