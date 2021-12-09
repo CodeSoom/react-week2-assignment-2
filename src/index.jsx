b6607b58c67b11e3287a3ffdbfcb84170de90e88
eunseo-kim <67737432+eunseo-kim@users.noreply.github.com>
2021-12-09 23:14:25 +0900

    todo 추가, 삭제 기능 구현 - [할 일을 입력하고 추가를 누르면 input의 텍스트가 지워진다] 구현 필요 - 컴포넌트 파일 분리 전

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,98 @@
 import { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+function Button({ children, onClick }) {
+  return (
+    <button type="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
+function Todo({ children, onClick }) {
+  return (
+    <div>
+      <span>{children}</span>
+      <Button onClick={onClick}>
+        완료
+      </Button>
+    </div>
+  );
+}
+
+function Todos({ todos, onClick }) {
+  if (todos.length === 0) {
+    return (
+      <div>
+        할 일이 없어요!
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {todos.map((todo, index) => (
+        <Todo
+          key={todo}
+          onClick={() => onClick(index)}
+        >
+          {index + 1}
+          .
+          {todo}
+        </Todo>
+      ))}
+    </div>
+
+  );
+}
+
+function Input({ onClick }) {
+  const [todo, setTodo] = useState('');
+  function handleChange(input) {
+    setTodo(input);
+  }
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="할 일을 입력해주세요."
+        onChange={(event) => handleChange(event.target.value)}
+      />
+
+      <Button onClick={() => onClick(todo)}>
+        추가
+      </Button>
+    </div>
+
+  );
+}
+
 function App() {
-  const [text, setText] = useState('Todo App!');
-  return <h1>{text}</h1>;
+  const [todos, setTodos] = useState([]);
+  function addTodo(newTodo) {
+    setTodos([...todos, newTodo]);
+  }
+
+  function deleteTodo(index) {
+    setTodos([...todos.slice(0, index), ...todos.slice(index + 1)]);
+  }
+
+  return (
+    <div>
+      <h1>To-do</h1>
+      <div>
+        <Input
+          onClick={addTodo}
+        />
+
+        <Todos
+          todos={todos}
+          onClick={deleteTodo}
+        />
+      </div>
+    </div>
+  );
 }
 
 ReactDOM.render(
