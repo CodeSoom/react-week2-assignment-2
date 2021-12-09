0bd95f00008a15224e4bd2b386c65f4af596e5fb
shongs27 <shongs27@naver.com>
2021-12-09 21:13:23 +0900

    Fix - index파일과 <App /> 분리

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import TodoInput from './TodoInput';
+import TodoList from './TodoList';
+
+export default function App() {
+  const [text, setText] = useState('');
+  function onChangeText(e) {
+    setText(e.target.value);
+  }
+
+  const [submitText, setSubmitText] = useState([]);
+  function onClickSubmit() {
+    setSubmitText((prev) => ([...prev, text]));
+    setText('');
+  }
+
+  function onClickFinish(t) {
+    setSubmitText((prevTexts) => prevTexts.filter((prev) => prev !== t));
+  }
+
+  return (
+    <>
+      <TodoInput text={text} onChange={onChangeText} onClick={onClickSubmit} />
+      <TodoList texts={submitText} onClick={onClickFinish} />
+    </>
+  );
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,30 +1,4 @@
-import { useState } from 'react';
 import ReactDOM from 'react-dom';
-import TodoInput from './TodoInput';
-import TodoList from './TodoList';
-
-function App() {
-  const [text, setText] = useState('');
-  function onChangeText(e) {
-    setText(e.target.value);
-  }
-
-  const [submitText, setSubmitText] = useState([]);
-  function onClickSubmit() {
-    setSubmitText((prev) => ([...prev, text]));
-    setText('');
-  }
-
-  function onClickFinish(t) {
-    setSubmitText((prevTexts) => prevTexts.filter((prev) => prev !== t));
-  }
-
-  return (
-    <>
-      <TodoInput text={text} onChange={onChangeText} onClick={onClickSubmit} />
-      <TodoList texts={submitText} onClick={onClickFinish} />
-    </>
-  );
-}
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
