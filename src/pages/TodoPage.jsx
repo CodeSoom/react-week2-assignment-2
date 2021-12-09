c670f62085fe8a9ab2a34141a1dd9b06c68da7ea
wallababa <ansrlansrl@naver.com>
2021-12-09 22:43:42 +0900

    todo 완료

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -4,17 +4,25 @@ import TodoList from '../components/TodoList';
 
 export default function TodoPage() {
  const [todos, setTodos] = useState([]);
+
  const handleAppend = (todo) => {
   setTodos((prev) => {
    return [...prev, todo];
   });
  };
 
+ const handleComplete = (index) => {
+  const result = [...todos];
+
+  result.splice(index, 1);
+  setTodos(result);
+ };
+
  return (
   <main>
    <h1>Todo</h1>
    <TodoAppending onAppend={handleAppend} />
-   <TodoList todos={todos} onComplete={(index) => { console.log('index', index) }} />
+   <TodoList todos={todos} onComplete={handleComplete} />
   </main>
  );
 }
