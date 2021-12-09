71800f87f10c6e416ca1f09434333f668b3b0b1b
syaring <sarah.k.serim@gmail.com>
2021-12-09 22:30:24 +0900

    Add UI elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
 
 export default function App() {
+  const todoList = [ // 임시
+    '아이폰 사기',
+    '맥북 프로 사기',
+    '에어팟 사기',
+    '애플 주식 사기',
+  ];
+
   return(
     <div>
-      Hello World!
+      <h2>To-do</h2>
+      <form>
+        <input />
+        <button type="submit">
+          추가
+        </button>
+      </form>
+      <div>
+        <ul>
+          {todoList.map((todoThing, i) => (
+            <li key={i}>
+              <span>
+                {i + 1}. {todoThing}
+              </span>
+              <button type="button">
+                삭제
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
