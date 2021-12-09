cbb820c386ae53a5a9d2bb02b91640f067dedabb
CheolWoong Choi <bluesky4381@naver.com>
2021-12-09 22:12:34 +0900

    Remove ternary operator and show explicit todos component

--- src/Todos.jsx
+++ src/Todos.jsx
@@ -1,18 +1,22 @@
 function Todos({ todos, onRemoveTodo }) {
+  const isEmptyTodos = todos.length === 0;
+
+  if (isEmptyTodos) {
+    return (
+      <div>할 일이 없어요!</div>
+    );
+  }
+
   return (
     <div>
-      {todos.length ? (
-        todos.map((todo, todoIdx) => (
-          <div>
-            <span>
-              {`${todoIdx + 1}. ${todo} `}
-            </span>
-            <button type="button" onClick={() => onRemoveTodo(todoIdx)}>완료</button>
-          </div>
-        ))
-      ) : (
-        <div>할 일이 없어요!</div>
-      )}
+      {todos.map((todo, todoIdx) => (
+        <div>
+          <span>
+            {`${todoIdx + 1}. ${todo} `}
+          </span>
+          <button type="button" onClick={() => onRemoveTodo(todoIdx)}>완료</button>
+        </div>
+      ))}
     </div>
   );
 }
