1249619011bcd4e929a39355f019f1bfcc0f9963
jnny1017 <jnny1017@gmail.com>
2021-12-09 23:17:39 +0900

    TodoList컴포넌트에서 삼항연산자 대신 guard clauses를 사용하여 수정

--- src/todoList.jsx
+++ src/todoList.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
 
 export default function TodoList({ array = [], handleClickDone }) {
+  if (array.length === 0) {
+    return <p>할 일이 없어요!</p>;
+  }
+
   return (
-    <>
-      {array.length === 0
-        ? <p>할 일이 없어요!</p>
-        : (
-          <ol>
-            {array.map((item) => (
-              <li key={item}>
-                {item}
-                <button
-                  type="button"
-                  onClick={() => handleClickDone(item)}
-                >
-                  완료
-                </button>
-              </li>
-            ))}
-          </ol>
-        )}
-    </>
+    <ol>
+      {array.map((item) => (
+        <li key={item}>
+          {item}
+          <button
+            type="button"
+            onClick={() => handleClickDone(item)}
+          >
+            완료
+          </button>
+        </li>
+      ))}
+    </ol>
   );
 }
