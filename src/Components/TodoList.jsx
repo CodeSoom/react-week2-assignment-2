6b04255e16750243573d8ff1648298dd52ffa140
jeongseop.lee <sjl0614@naver.com>
2021-12-10 20:22:46 +0900

    :recycle: TodoList 예외 케이스 처리 변경

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -1,17 +1,17 @@
-const TodoList = ({ todos, onClick }) => (
-  <>
-    {Array.isArray(todos) && todos.length > 0 ? (
-      <ul style={{ listStyleType: 'none', padding: 0 }}>
-        {todos.map(({ todo, id }, index) => (
-          <li key={id} style={{ padding: '5px 0' }}>
-            {`${index + 1}. ${todo}`}
-            <button type="button" onClick={() => onClick(id)}>완료</button>
-          </li>
-        ))}
-      </ul>
-    )
-      : <p>할 일이 없어요!</p>}
-  </>
-);
+const TodoList = ({ todos, onClick }) => {
+  if (!Array.isArray(todos) || (Array.isArray(todos) && todos.length === 0)) {
+    return <p>할 일이 없어요!</p>;
+  }
+  return (
+    <ul style={{ listStyleType: 'none', padding: 0 }}>
+      {todos.map(({ todo, id }, index) => (
+        <li key={id} style={{ padding: '5px 0' }}>
+          {`${index + 1}. ${todo}`}
+          <button type="button" onClick={() => onClick(id)}>완료</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default TodoList;
