3fe36942ba8391934c407c1275a4ce8e3c27d930
emewjin <emewjin@gmail.com>
2021-12-10 22:21:55 +0900

    ref: 불필요한 이벤트 핸들러 함수 삭제

--- src/ToDoInputField.jsx
+++ src/ToDoInputField.jsx
@@ -3,23 +3,20 @@ import { useState } from 'react';
 export default function ToDoInputField({ appendToDoHandler }) {
   const [newToDoValue, setNewToDoValue] = useState('');
 
-  const onClickAppendToDo = () => {
-    appendToDoHandler(newToDoValue);
-    setNewToDoValue('');
-  };
-
   const onChangeInput = ({ target: { value } }) => {
     setNewToDoValue(value);
   };
 
   const onSubmitNewToDo = (event) => {
     event.preventDefault();
+    appendToDoHandler(newToDoValue);
+    setNewToDoValue('');
   };
 
   return (
     <form onSubmit={onSubmitNewToDo}>
       <input value={newToDoValue} placeholder="할 일을 입력해 주세요" onChange={onChangeInput} />
-      <button onClick={onClickAppendToDo} type="submit">
+      <button type="submit">
         추가
       </button>
     </form>
