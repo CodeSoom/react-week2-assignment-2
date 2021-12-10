08630ffb69d11a0fa5a1b805d5495e1d1f678e03
padawanR0k <35283339+padawanR0k@users.noreply.github.com>
2021-12-10 12:39:19 +0900

    Update src/components/TodoItem/index.jsx
    Co-authored-by: Yunseok <hannut91@gmail.com>

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -1,7 +1,7 @@
 const TodoItem = ({ content, onClick }) => (
   <div>
     {content}
-    <button onClick={() => onClick()} type="button">
+    <button onClick={onClick} type="button">
       완료
     </button>
   </div>
