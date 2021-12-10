ed5009a7820dc08cdb57a7ee27faa557a9fe352e
padawanR0k <35283339+padawanR0k@users.noreply.github.com>
2021-12-10 12:46:27 +0900

    up: update Todo component

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -1,15 +1,21 @@
 import TodoItem from '../TodoItem';
 
-const ToDoList = ({ list, completeTodo }) => (
-  <div>
-    {list.map((todo) => (
-      <TodoItem
-        key={todo.id}
-        content={todo.content}
-        onClick={() => completeTodo(todo.id)}
-      />
-    ))}
-  </div>
-);
+const ToDoList = ({ list, completeTodo }) => {
+  if (list.length === 0) {
+    return '할 일이 없어요!'
+  }
+
+  return (
+    <div>
+      {list.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          content={todo.content}
+          onClick={() => completeTodo(todo.id)}
+        />
+      ))}
+    </div>
+  );
+}
 
 export default ToDoList;
