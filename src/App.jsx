837cfdfc882b1c65eb3f7a7297ec02cd1d76c9a0
emewjin <emewjin@gmail.com>
2021-12-10 21:19:46 +0900

    ref: list state 네이밍 수정

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,21 @@ import { useState } from 'react';
 import ToDo from './ToDo';
 
 export default function App() {
-  const [list, setList] = useState([]);
+  const [toDos, setToDos] = useState([]);
 
   const onClickAppendToDo = (text) => {
-    setList([...list, { id: Date.now() + text, text }]);
+    setToDos([...toDos, { id: Date.now() + text, text }]);
   };
 
   const onClickRemoveToDo = (id) => {
-    setList(list.filter((item) => item.id !== id));
+    setToDos(toDos.filter((item) => item.id !== id));
   };
 
   return (
     <ToDo
       appendHandler={onClickAppendToDo}
       removeHandler={onClickRemoveToDo}
-      list={list}
+      toDos={toDos}
     />
   );
 }
