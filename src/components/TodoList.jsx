b1053a6fcabf033fc9fbadc736135e9b85ab7900
yeji <lerry597@gmail.com>
2021-12-11 11:29:25 +0900

    Refactor ternary operator

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,21 +1,20 @@
 import Button from './Button';
 
 export default function TodoList({ todoArray, handleFinishTodo }) {
-  return (
-    <>
-      {todoArray.length === 0 ? <p>할 일이 없어요!</p>
-        : (
-          <ol>
-            {todoArray.map((todoItem) => (
-              <li key={todoItem}>
-                {todoItem}
-                <Button onClick={() => handleFinishTodo(todoItem)}>
-                  완료
-                </Button>
-              </li>
-            ))}
-          </ol>
-        )}
-    </>
-  );
+  if (todoArray.length === 0) return <p>할 일이 없어요!</p>
+  else if (todoArray.length !== 0) {
+    return (
+      <ol>
+        {todoArray.map((todoItem) => (
+          <li key={todoItem}>
+            {todoItem}
+            <Button onClick={() => handleFinishTodo(todoItem)}>
+              완료
+            </Button>
+          </li>
+        ))}
+      </ol>
+    )
+  }
 }
+
