104169ecf634bc5ae91207a48b5224d0fd3d4859
Dyel0216 ParkDyel <bluetibe@gmail.com>
2021-12-14 23:54:20 +0900

    fix(ParkDyel) Refactoring
     - Rename: todoList -> todos

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,15 @@ import React, { useState } from 'react';
 import Page from './Page';
 
 export default function App() {
-  const [todoList, setTodoList] = useState([]);
+  const [todos, setTodos] = useState([]);
 
   const onInsert = (text) => {
-    setTodoList((oldArray) => [
-      ...oldArray,
-      { key: todoList.length + 1, text },
-    ]);
+    setTodos((oldArray) => [...oldArray, { key: todos.length + 1, text }]);
   };
 
   const onFinish = (key) => {
-    setTodoList(todoList.filter((item) => item.key !== key));
+    setTodos(todos.filter((item) => item.key !== key));
   };
 
-  return <Page list={todoList} onInsert={onInsert} onFinish={onFinish} />;
+  return <Page todos={todos} onInsert={onInsert} onFinish={onFinish} />;
 }
--- src/Page.jsx
+++ src/Page.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 import TodoList from './TodoList';
 import Insert from './Insert';
 
-export default function Page({ list, onInsert, onFinish }) {
+export default function Page({ todos, onInsert, onFinish }) {
   return (
     <div>
       <h1>To-do</h1>
       <Insert onClick={onInsert} placeholder="할 일을 입력해 주세요" />
-      <TodoList list={list} onClick={onFinish} />
+      <TodoList todos={todos} onClick={onFinish} />
     </div>
   );
 }
