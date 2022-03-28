3f6ca5c0bf17cbf700befc32d306cf737b91b391
이찬호 <0407chan@naver.com>
2022-03-28 16:35:34 +0900

    todolist 컴포넌트 생성

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
-function ToDoList() {
-  return <>TodoList</>;
+function ToDoList({ todo, onRemoveTask }) {
+  if (todo.length === 0) {
+    return '할 일이 없어요!';
+  }
+
+  return (
+    <>
+      {todo.map((item, index) => (
+        <div>
+          {index + 1}
+          .
+          {' '}
+          {item}
+          <button type="button" onClick={() => onRemoveTask(index)}>완료</button>
+        </div>
+      ))}
+    </>
+  );
 }
 
 export default ToDoList;
