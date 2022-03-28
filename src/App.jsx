3c4b120041e802421a8a588a5e68194240894c15
이찬호 <0407chan@naver.com>
2022-03-28 16:35:43 +0900

    app 컴포넌트 생성

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useState } from 'react/cjs/react.production.min';
+import ToDoList from './ToDoList';
+
+function App() {
+  const [todo, setTodo] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  function onNewTask(event) {
+    setNewTask(event.target.value);
+  }
+
+  function onAddTask() {
+    setTodo([...todo, newTask]);
+    setNewTask('');
+  }
+
+  function onRemoveTask(index) {
+    setTodo(todo.filter((item, i) => i !== index));
+  }
+
+  return (
+    <>
+      <h1>To-do</h1>
+      <input placeholder="할 일을 입력해 주세요" value={newTask} onChange={onNewTask} />
+      <button type="button" onClick={onAddTask}>추가</button>
+      <p>
+        <ToDoList todo={todo} onRemoveTask={onRemoveTask} />
+      </p>
+    </>
+  );
+}
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ToDoList from './ToDoList';
+import App from './App';
 
-ReactDOM.render(<ToDoList />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
