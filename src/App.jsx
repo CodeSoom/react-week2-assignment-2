fedfde3efcbc81338f963ce556a92cc80a518288
0hu <dladudgn123@naver.com>
2022-03-30 20:38:52 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
 import { useState } from 'react';
 
 export default function App() {
-  const [value, setValue] = useState('');
+  const [text, setText] = useState('');
 
   const onClick = (e) => {
     // TODO: 클릭시 todo list 추가 해야함.
+    setText('');
     e.preventDefault();
   };
 
   const onChange = (e) => {
-    setValue({
-      ...value,
-      value: e.target.value,
-    });
+    console.log(e.target.value);
+    setText(e.target.value);
   };
 
   return (
     <>
       <h1>To-do</h1>
-        <input onChange={onChange} placeholder="할 일을 입력해 주세요" />
-        <button onClick={onClick}>추가</button>
+      <input type="text" onChange={onChange} value={text} placeholder="할 일을 입력해 주세요" />
+      <button type="submit" onClick={onClick}>추가</button>
     </>
   );
 }
