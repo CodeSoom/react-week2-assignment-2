6b0e0830ea9c5aeb985d29893ac1f3f6c96b03a0
0hu <dladudgn123@naver.com>
2022-03-30 18:30:48 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import { useState } from 'react';
 export default function App() {
   const [value, setValue] = useState('');
 
-  const handleSubmit = (e) => {
-    // TODO: todo list 추가 해야함.
+  const onClick = (e) => {
+    // TODO: 클릭시 todo list 추가 해야함.
     e.preventDefault();
   };
 
-  const handleChange = (e) => {
+  const onChange = (e) => {
     setValue({
       ...value,
       value: e.target.value,
@@ -18,12 +18,8 @@ export default function App() {
   return (
     <>
       <h1>To-do</h1>
-      <form onSubmit={() => handleSubmit}>
-        <label>
-          <textarea value={value} onChange={() => handleChange} />
-        </label>
-        <input type="submit" value="추가" />
-      </form>
+        <input onChange={onChange} placeholder="할 일을 입력해 주세요" />
+        <button onClick={onClick}>추가</button>
     </>
   );
 }
