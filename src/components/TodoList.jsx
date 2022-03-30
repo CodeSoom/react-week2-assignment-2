9b91551160b9e67faca38334ad124a4a67dec690
Jiwoo Choi <heyjiwoo@annotation.ai.com>
2022-03-31 07:10:59 +0900

    Use guard clause
    Use guard clause.

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,19 +1,19 @@
 import Todo from './Todo';
 
 export default function TodoList({ todos, onClickDelete }) {
+  if (todos.length > 0) {
+    return (
+      <ol>
+        {todos.map((todo) => (
+          <Todo todo={todo} onClickDelete={() => onClickDelete(todo.id)} />
+        ))}
+      </ol>
+    );
+  }
+
   return (
-    <>
-      {todos.length > 0 ? (
-        <ol>
-          {todos.map((todo) => (
-            <Todo todo={todo} onClickDelete={() => onClickDelete(todo.id)} />
-          ))}
-        </ol>
-      ) : (
-        <p>
-          할 일이 없어요!
-        </p>
-      )}
-    </>
+    <p>
+      할 일이 없어요!
+    </p>
   );
 }
