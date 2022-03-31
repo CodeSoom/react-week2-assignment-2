5d633aaaff11f694748fa6283178dfc5efe45e29
0hu <dladudgn123@naver.com>
2022-03-31 21:59:17 +0900

    Fixing

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,12 +1,4 @@
 export default function ToDoList({ todos, onClick }) {
-  // 아무일도 없으면 할일이 없어요 출력  if (!어레이) 할일이 없어요 출력
-  // 추가를 눌렀을때 할일 추가  추가 누르면 어레이 뒤에 추가
-  // 추가 되면  1 . 할일 이런식으로 숫자와 할일 보여주기 2. 3 .4 이런식으로
-  // 완료 버튼을 누르면 할일을 삭제.   완료버튼 누르면 삭제 .. 필터써야하나??
-
-  if (!todos) {
-    return <p> 할일이 없어요!</p>;
-  }
   if (todos) {
     return (
       todos.map((todo, id) => (
@@ -24,5 +16,8 @@ export default function ToDoList({ todos, onClick }) {
         </li>
       ))
     );
+  }
+  if (!todos) {
+    return <p> 할일이 없어요!</p>;
   }
 }
