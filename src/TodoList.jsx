1237fd23b2aee8cd3970d661a7449eb57cdf5b93
free-ko <duddnr654@naver.com>
2022-03-31 21:04:16 +0900

    [2-2] Fix: TodoList guard clauses 형태로 예외처리 적용

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,21 +1,17 @@
 import TodoItem from './TodoItem';
 
 export default function TodoList({ todoList, onDelete }) {
+  if (todoList.length === 0) {
+    return <p>할 일이 없어요!</p>;
+  }
+
   return (
     <div>
-      {todoList.length > 0 ? (
-        <ol>
-          {todoList.map((todoItem) => (
-            <TodoItem
-              key={todoItem.id}
-              todoItem={todoItem}
-              onDelete={onDelete}
-            />
-          ))}
-        </ol>
-      ) : (
-        <p>할 일이 없어요!</p>
-      )}
+      <ol>
+        {todoList.map((todoItem) => (
+          <TodoItem key={todoItem.id} todoItem={todoItem} onDelete={onDelete} />
+        ))}
+      </ol>
     </div>
   );
 }
