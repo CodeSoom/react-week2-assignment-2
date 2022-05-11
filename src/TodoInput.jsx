87335945ec5bcbd26932685864b29492fffc73d1
ByeongSooLee-KR <djaxornwkd12@gmail.com>
2022-05-11 21:39:23 +0900

    Refactor: form 태그 안에 button type 'submit' 추가

--- src/TodoInput.jsx
+++ src/TodoInput.jsx
@@ -4,7 +4,7 @@ export default function TodoInput({ todoInput, onChange, onSubmit }) {
   return (
     <form onSubmit={onSubmit}>
       <input onChange={onChange} value={todoInput.text} />
-      <button type="button">추가</button>
+      <button type="submit">추가</button>
     </form>
   );
 }
