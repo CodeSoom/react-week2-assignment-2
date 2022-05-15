c489d9f5d0991dce7431401790c74518eddc14b7
jodheeee <soso99700@naver.com>
2022-05-15 13:03:24 +0900

    목록이 없을때 예외 케이스를 명확하게 두고 빠른 코드 종료 적용

--- src/Todo/Todos.jsx
+++ src/Todo/Todos.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
 
 export default function Todos({ todos, onClick }) {
+  const isEmpty = (arr = []) => arr.length === 0;
+
+  if (isEmpty(todos)) {
+    return <p>할 일이 없어요!</p>;
+  }
+
   return (
     <div>
-      {todos.length > 0
-        ? (todos.map((todo, i) => (
-          <div
-            key={todo.idx}
-            style={{
-              margin: '10px 0 0 20px',
-            }}
-          >
-            <span>
-              {i + 1}
-              {'. '}
-              {todo.work}
-            </span>
-            <button type="button" onClick={() => onClick(todo.idx)}>
-              완료
-            </button>
-          </div>
-        )))
-        : (<p>할 일이 없어요!</p>)}
+      {todos.map((todo, i) => (
+        <div
+          key={todo.idx}
+          style={{
+            margin: '10px 0 0 20px',
+          }}
+        >
+          <span>
+            {i + 1}
+            {'. '}
+            {todo.work}
+          </span>
+          <button type="button" onClick={() => onClick(todo.idx)}>
+            완료
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
