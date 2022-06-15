64916f3860c3bd2f8545042ac234aa38943646d6
yerin512 <73337811+yerin512@users.noreply.github.com>
2022-06-15 20:57:08 +0900

    feat: todo-list 화면을 추가했습니다

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,12 +1,24 @@
-export default function TodoList() {
+export default function TodoList({ data, onClick }) {
   const style = { display: 'flex', alignItems: 'center' };
 
   return (
-    <div>
-      <div style={style}>
-        <p>1. 할일</p>
-        <button type="button">완료</button>
-      </div>
-    </div>
+    <>
+      {data.length < 1 ? (
+        <p>할 일을 등록해주세요.</p>
+      ) : (
+        <div>
+          {data.map((item) => (
+            <div style={style} key={item.idx}>
+              <p>
+                {item.idx}
+                .
+                {item.toDo}
+              </p>
+              <button type="button" onClick={() => onClick(item)}>완료</button>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
   );
 }
