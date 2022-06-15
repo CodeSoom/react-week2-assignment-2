9594fa9acc4c211bddb0dcb323668e62937b7a74
yerin512 <73337811+yerin512@users.noreply.github.com>
2022-06-15 20:56:44 +0900

    feat: 새로운 todo 입력 기능을 추가했습니다

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
+import { useState } from 'react';
+
 import Title from './components/Title';
 import InputTodo from './components/InputTodo';
 import TodoList from './components/TodoList';
 
 export default function App() {
+  const [data, setData] = useState([]);
+  const [createIdx, setCreateIdx] = useState(1);
+
+  const addToDo = (inputToDo) => {
+    setCreateIdx(createIdx + 1);
+
+    const newTodo = {
+      idx: createIdx,
+      toDo: inputToDo,
+    };
+
+    setData([newTodo, ...data]);
+  };
+
+  const completeToDo = () => {
+  // completedToDo 삭제하는 코드 작성 예정
+  };
+
   return (
     <>
       <Title title="To-do" />
-      <InputTodo />
-      <TodoList />
+      <InputTodo onClick={addToDo} />
+      <TodoList data={data} onClick={completeToDo} />
     </>
   );
 }
--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,8 +1,26 @@
-export default function InputTodo() {
+import { useState } from 'react';
+
+export default function InputTodo({ onClick }) {
+  const [newTodo, setNewTodo] = useState('');
+
+  const handleInputChange = (e) => {
+    setNewTodo(e.target.value);
+  };
+
   return (
     <div>
-      <input type="text" placeholder="할 일을 입력해주세요." />
-      <button type="button">추가</button>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={handleInputChange}
+        placeholder="할 일을 입력해주세요."
+      />
+      <button
+        type="button"
+        onClick={() => onClick(newTodo)}
+      >
+        추가
+      </button>
     </div>
   );
 }
