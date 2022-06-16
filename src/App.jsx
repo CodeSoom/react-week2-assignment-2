54b6652339b4f65c6a060778224a653fad678944
kyh196201 <kyh196201@gmail.com>
2022-06-16 20:38:20 +0900

    feat: 새로운 todoItem 추가하는 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,26 @@ import TodoForm from './TodoForm';
 export default function App() {
   const [todoItems, setTodoItems] = useState([]);
 
+  function addTodoItem(todoItem) {
+    const newTodoItems = [...todoItems, todoItem];
+
+    setTodoItems(newTodoItems);
+  }
+
+  function handleSubmitTodoForm(todoTitle = '') {
+    const todoItem = {
+      id: Date.now(),
+      title: todoTitle,
+    };
+
+    addTodoItem(todoItem);
+  }
+
   return (
     <div>
       <h1>To-do</h1>
 
-      <TodoForm />
+      <TodoForm onSubmit={handleSubmitTodoForm} />
 
       <TodoItems todoItems={todoItems} />
     </div>
--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,7 +1,30 @@
-export default function TodoForm() {
+import { useState } from 'react';
+
+export default function TodoForm({ onSubmit }) {
+  const [todoTitle, setTodoTitle] = useState('');
+
+  function clearTodoTitle() {
+    setTodoTitle('');
+  }
+
+  function handleChangeTodoTitle(e) {
+    setTodoTitle(e.target.value.trim());
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // @TODO alert로 변경하기
+    if (!todoTitle) return;
+
+    onSubmit(todoTitle);
+
+    clearTodoTitle();
+  }
+
   return (
-    <form>
-      <input type="text" />
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={todoTitle} onChange={handleChangeTodoTitle} />
       <button type="submit">추가</button>
     </form>
   );
