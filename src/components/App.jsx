8bb54fdf92bebd5fdaf89954a0491f6c9f98e5a4
Nakyeon Ko <59277499+nakyeonko3@users.noreply.github.com>
2022-06-16 21:11:45 +0900

    updated >App.jsx 클릭시 버튼 번호를 출력하는 기능 구현

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,13 @@ import Page from './Page';
 
 const handleSubmit = (e) => {
   e.preventDefault();
-  console.log(e.target.firstChild.value);
+  console.log(`'${e.target.firstChild.value}', 해야 할 일 목록 업데이트`);
   e.target.firstChild.value = '';
 };
 
 const handleClick = (e) => {
   e.preventDefault();
-  console.log(e.target.parentNode);
+  console.log(`${e.target.className}번 항목 완료됨`);
 };
 
 export default function App() {
--- src/components/OrderedList.jsx
+++ src/components/OrderedList.jsx
@@ -2,9 +2,15 @@ export default function OrderedList({ onClick }) {
   return (
     <ol>
       {[1, 2, 3, 4, 5].map((i) => (
-        <li>
+        <li key={i}>
           <span>항목</span>
-          <input onClick={onClick} type="button" value="완료" />
+          <input
+            className={i}
+            key={i}
+            onClick={onClick}
+            type="button"
+            value="완료"
+          />
         </li>
       ))}
     </ol>
