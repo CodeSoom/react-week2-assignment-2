076028d12d44a299275ec6d0e77d3cc780bcd25d
kyh196201 <kyh196201@gmail.com>
2022-06-16 20:12:32 +0900

    fix: className 제거

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,12 @@ export default function App() {
   const [todoItems, setTodoItems] = useState([]);
 
   return (
-    <div className="container">
-      <h1 className="title">To-do</h1>
+    <div>
+      <h1>To-do</h1>
       <TodoForm />
 
       {
-        todoItems.length ? (<TodoItems todoItems={todoItems} />) : (<p className="todo-empty">할 일을 입력해주세요</p>)
+        todoItems.length ? (<TodoItems todoItems={todoItems} />) : (<p>할 일을 입력해주세요</p>)
       }
     </div>
   );
--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,8 +1,8 @@
 export default function TodoForm() {
   return (
-    <form className="todo-form">
-      <input type="text" className="todo-input" />
-      <button type="submit" className="todo-submit">추가</button>
+    <form>
+      <input type="text" />
+      <button type="submit">추가</button>
     </form>
   );
 }
