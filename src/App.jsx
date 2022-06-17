3460104cfdd5006a86569f4e7a4878280f15892c
2scent <ad7549@gmail.com>
2022-06-17 18:46:19 +0900

    Use useRef() to save nextId

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 import Title from './Title';
 import TodoForm from './TodoForm';
@@ -6,11 +6,11 @@ import TodoList from './TodoList';
 
 const App = () => {
   const [todos, setTodos] = useState([]);
-  const [nextId, setNextId] = useState(1);
+  const nextId = useRef(1);
 
   const addTodo = (title) => {
-    setTodos([...todos, { id: nextId, title }]);
-    setNextId(nextId + 1);
+    setTodos([...todos, { id: nextId.current, title }]);
+    nextId.current += 1;
   };
 
   const deleteTodo = (todoDelete) => {
