a12f2e15371c4ca1cfb80d94959e362c72daa068
yerin512 <73337811+yerin512@users.noreply.github.com>
2022-06-18 19:27:40 +0900

    엔터 입력시 할 일이 추가되는 걸 form 태그로 리팩토링해라
    기존에는 엔터 입력시 할 일 추가하는 걸 e.key를 이용했지만
    시맨틱한 태그를 사용하는 form 태그로 코드를 리팩토링했습니다.

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -3,25 +3,24 @@ export default function InputTodo({ onClick, inputTodo, setInputTodo }) {
     setInputTodo(e.target.value);
   };
 
-  const handleEnter = (e) => {
-    if (e.key === 'Enter') onClick(e.target.value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onClick(inputTodo);
   };
 
   return (
-    <div>
+    <form onSubmit={handleSubmit}>
       <input
         type="text"
         value={inputTodo}
         onChange={handleInputChange}
-        onKeyPress={handleEnter}
         placeholder="할 일을 입력해주세요."
       />
       <button
-        type="button"
-        onClick={() => onClick(inputTodo)}
+        type="submit"
       >
         추가
       </button>
-    </div>
+    </form>
   );
 }
