243eaca405b4fb6a6a17623820d654dec732fb76
Heejae Chang <heejae0704@gmail.com>
2022-07-11 16:52:44 +0900

    p element inside to-do components has id attribute
    key는 이미 React Component인 <ToDo /> 레벨에서 붙였으므로 그 안의 p 요소에는
    id attribute를 심어줌

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Todo({ index, toDo, onCompleteButtonClick }) {
   return (
-    <p key={index.toString()}>
+    <p id={index.toString()}>
       {`${index + 1}. ${toDo}`}
       <button type="button" onClick={() => onCompleteButtonClick(index)}>완료</button>
     </p>
