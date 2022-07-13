80a13373cb2064be2aac80c3895e2eb5596fa77d
hanna244 <shhn0509@gmail.com>
2022-07-13 21:40:05 +0900

    update: label 태그 수정 및 form 태그 추가

--- src/Input.jsx
+++ src/Input.jsx
@@ -5,11 +5,14 @@ import CommonButton from './CommonButton';
 export default function Input({ handleClick, handleChange, inputTextValue }) {
   return (
     <div>
-      <label htmlFor="todoInput">
+      <form>
+        <label htmlFor="todoInput" style={{ fontSize: 0 }}>
+          오늘 할 일 입력
+        </label>
         <input id="todoInput" type="text" onChange={handleChange} />
         {/* disabled={inputTextValue == '' && true} → 인풋에 입력 값이 없을 경우 disabled 속성이 활성화 되도록 조건처리 */}
         <CommonButton lable="추가" handleClick={handleClick} disabled={inputTextValue === '' && true} />
-      </label>
+      </form>
     </div>
   );
 }
