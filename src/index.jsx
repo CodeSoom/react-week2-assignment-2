7a8d92f196443a6f8b9cb5952f77b289f69cf62a
moon-kun86 <108505117+moon-kun86@users.noreply.github.com>
2022-07-14 17:01:04 +0900

    To-do 리스트 만들기 2일차입니다.
    오늘은 해당 input의 값이 change될때 event target을 가져와서 value을 set준 다음 클릭했을때 가져올 수 있는지 테스트를 해보았습니다.
    다음 업데이트 할때에는 해당 value값을 배열에 저장하여 리스트로 나올 수 있도록 구현예정입니다.

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,42 @@
 /* @jsx React.createElement */
 
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 // 추가하기 컴포넌트
+function addList(i) {
+    // console.log(i);
 
+}
+
+// input 값 입력받기
+function AddInput(){
+    
+    const [text, setText] = useState('');
+
+    const onChange = (e) => {
+        console.log(e.target);
+
+        // console.log(e.target.value);
+
+        setText(e.target.value); // 인풋의 벨류값을 set 해줍니다.
+    }
+    
+    const onClick = () => {
+        console.log(text); // 입력 받은 value 값을 클릭할때 가져옵니다.
+    }
+
+    return (
+        <div>
+            <div>
+                <input type="text" onChange={onChange} value={text} placeholder="할 일을 입력해주세요." />
+            </div>
+            <div>
+                <button type="button" onClick={onClick}>추가</button>        
+            </div>
+        </div>
+    )
+}
 
 // 삭제하기 컴포넌트
 
@@ -13,8 +45,7 @@ import ReactDOM from 'react-dom';
 function Page(){
     return (
         <div>
-            <input type="text" placeholder="할 일을 입력해주세요." />
-            <button type="button">추가</button>
+            <AddInput />
         </div>
     )
 }
