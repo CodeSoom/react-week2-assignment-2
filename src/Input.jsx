5046d1fb4667e2faec2f25c3b6485eb79a28eff9
hanna244 <shhn0509@gmail.com>
2022-07-14 20:59:47 +0900

    update: 빈문자열인 경우 추가 버튼 비활성화

--- src/Input.jsx
+++ src/Input.jsx
@@ -11,7 +11,7 @@ export default function Input({ handleClick, handleChange, inputTextValue }) {
         </label>
         <input id="todoInput" type="text" onChange={handleChange} />
         {/* disabled={inputTextValue == '' && true} → 인풋에 입력 값이 없을 경우 disabled 속성이 활성화 되도록 조건처리 */}
-        <CommonButton handleClick={handleClick} disabled={inputTextValue === '' && true}>
+        <CommonButton handleClick={handleClick} disabled={!inputTextValue.trim()}>
           추가
         </CommonButton>
       </form>
