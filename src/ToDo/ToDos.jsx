226b4c512d02f758853e3bbb1300b6ae7f9da94a
ChoJinmok <jjmok1127@gmail.com>
2022-08-10 20:21:34 +0900

    적용안됐던 eslint 적용

--- src/ToDo/ToDos.jsx
+++ src/ToDo/ToDos.jsx
@@ -1,12 +1,12 @@
 export default function ToDos({ toDos, onDeleteButtonClick }) {
-    return (
-      <ol>
-        {toDos.map((toDo) => (
-          <li key={toDo.id}>
-            <span>{toDo.content}</span>
-            <button type="button" onClick={() => onDeleteButtonClick(toDo.id)}>완료</button>
-          </li>
-        ))}
-      </ol>
-    );
+  return (
+    <ol>
+      {toDos.map((toDo) => (
+        <li key={toDo.id}>
+          <span>{toDo.content}</span>
+          <button type="button" onClick={() => onDeleteButtonClick(toDo.id)}>완료</button>
+        </li>
+      ))}
+    </ol>
+  );
 }
