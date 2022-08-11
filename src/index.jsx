4dd38350e008e5c1e96d7042a9298669e7cca42e
SangYoonLee <sylagape1231@gmail.com>
2022-08-11 19:18:50 +0900

    # Week 2 Assignment 2 - '간단한 Todo App 만들기'
    - First Submit
    
    - 아직 과제 수행을 위한 공부 중이지만, 미리 PR을 보냅니다! (세팅 작업)

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+// import App from './App';
+
+function App() {
+  const [state, useState] = useState();
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
