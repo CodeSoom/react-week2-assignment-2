bdb2a1a5d99a01bab038c36949529675f1e5141b
0-Chan <kyc4421194@gmail.com>
2022-08-13 19:43:38 +0900

    Add '추가' button handler to add todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,53 @@
 import { useState } from 'react';
 
 export default function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: '리액트 시작',
-    },
-    {
-      id: 2,
-      text: '컴포넌트 이해',
-    },
-    {
-      id: 3,
-      text: '투두리스트 만들기',
-    },
-  ]);
-  const [text, setText] = useState('');
-
-  function handleAddClick() {
-    setTodos([
-      ...todos,
+  const [state, setState] = useState({
+    newId: 100,
+    todoTitle: '',
+    todos: [
       {
-        id: 0,
-        text: '',
-      }
-    ])
+        id: 1,
+        text: '리액트 시작',
+      },
+      {
+        id: 2,
+        text: '컴포넌트 이해',
+      },
+      {
+        id: 3,
+        text: '투두리스트 만들기',
+      },
+    ],
+  });
+
+  const { newId, todoTitle, todos } = state;
+
+  const [text, setText] = useState('')
+
+  function handleChangeTitle(e) {
+    setState({
+      ...state,
+      todoTitle: e.target.value,
+    });
   }
 
-  const todoLists = todos.map(todo =>
-    <li key={todo.id}>
-      {todo.text}
-      <button type="button">
-        완료
-      </button>
-    </li>
-  );
+  function handleAddClick() {
+    setState({
+      ...state,
+      newId: newId +1,
+      todos: [...todos, { id: newId, text: todoTitle }],
+    });
+  }
 
   return (
     <div>
       <h1>To-do</h1>
 
-      <input></input>
+      <input
+        type="text"
+        placeholder="할 일을 입력해 주세요"
+        onChange={handleChangeTitle}
+      />
       <button onClick={handleAddClick}>
         추가
       </button>
