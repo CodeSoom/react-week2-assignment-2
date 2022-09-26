56af262e564446f5f5ef1665eecd1cb63f1e1e0e
wjpark01 <wjpark01@fnfcorp.com>
2022-09-26 10:35:00 +0900

    주석 제거/ input tag 설정

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,6 @@ import TodoList from './TodoList';
 export default function App() {
   const [value, setValue] = useState('');
   const [toDoList, setTodoList] = useState([]);
-  // const [state, setState] = useState({
-  //   value: '',
-  //   toDoList: [],
-  // });
-  
   function insertTodo() {
     const tempTodoList = [...toDoList, value];
     setTodoList(tempTodoList);
@@ -17,17 +12,15 @@ export default function App() {
 
   function deleteTodo(index) {
     const tempTodoList = toDoList.filter((todo, i) => i !== index);
-    // setState({ ...state, toDoList: tempTodoList });
     setTodoList(tempTodoList);
   }
 
   return (
     <div>
       <h1>To-Do</h1>
-      <input value={value} onChange={(e) => { setValue( e.target.value ); }} />
+      <input type="text" name="toDoText" value={value} onChange={(e) => { setValue(e.target.value); }} />
       <button type="button" onClick={insertTodo}>추가</button>
       <TodoList toDoList={toDoList} onClick={deleteTodo} />
     </div>
   );
 }
-
