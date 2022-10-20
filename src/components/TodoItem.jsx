e5c2870f08f16396f88df68f30144d9290fe59d9
sejineeee <sejinee0918@gmail.com>
2022-10-20 17:35:36 +0900

    feat: todo item 구현
    차례를 매겨주는 숫자를 todoList의 index를 받아와 1을 더하여 구현
    완료 버튼을 누를 때 onClick 메서드에 id 값을 인자로 전달

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function TodoItem({ todoItem, index, onDelete }) {
+  const { id, content } = todoItem;
+
+  return (
+    <div>
+      <span>
+        {index + 1}
+        .
+      </span>
+      <span>{content}</span>
+      <button type="button" onClick={() => onDelete(id)}>완료</button>
+    </div>
+  );
+}
+
+export default TodoItem;
