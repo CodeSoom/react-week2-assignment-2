522f4197ea82633d68ef79a90411bbd31891deed
sejineeee <sejinee0918@gmail.com>
2022-10-20 17:34:13 +0900

    feat: todo editor 구현

---
+++ src/components/TodoEditor.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function TodoEditor({ todo, onChange, onClick }) {
+  const { content } = todo;
+  return (
+    <div>
+      <input
+        type="text"
+        value={content}
+        placeholder="할 일을 입력해주세요"
+        onChange={onChange}
+      />
+      <button type="button" onClick={() => onClick(content)}>추가</button>
+    </div>
+  );
+}
+
+export default TodoEditor;
