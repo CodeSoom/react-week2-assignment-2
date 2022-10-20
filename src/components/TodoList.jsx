6e7c37f48f353d7165b26e6965cc3fed96b0bb5e
sejineeee <sejinee0918@gmail.com>
2022-10-20 17:31:40 +0900

    feat: todo list 컴포넌트 구현
    todo list가 없을 경우 할 일이 없어요 메시지 나오게 구현
    todo list가 있을 경우 map 메서드를 이용

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import TodoItem from './TodoItem';
+
+function TodoList({ todoList, onDelete }) {
+  return (
+    <div>
+      {todoList.length === 0
+        ? <p>할 일이 없어요!</p>
+        : todoList.map((item, i) => (
+          <TodoItem
+            todoItem={item}
+            index={i}
+            todoList={todoList}
+            key={item.id}
+            onDelete={onDelete}
+          />
+        ))}
+    </div>
+  );
+}
+
+export default TodoList;
