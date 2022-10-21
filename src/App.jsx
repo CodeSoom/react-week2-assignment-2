e581dc579f0023a6830f842039e6ef7967a3d5a3
hyeji07 <zindex_ann@naver.com>
2022-10-21 16:47:34 +0900

    refactor:TodoPage삭제,handleSubmit 함수 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,50 @@
-import TodoPage from './TodoPage';
+import { useState } from 'react';
+
+import TodoForm from './TodoForm';
+import Todos from './Todos';
 
 function App() {
+  const [todoItems, setTodoItems] = useState([]);
+
+  const [todoText, setTodoText] = useState('');
+
+  const handleChange = (e) => setTodoText(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!todoText) {
+      // eslint-disable-next-line no-alert
+      alert('할 일을 입력해 주세요😀');
+      return;
+    }
+
+    setTodoItems([
+      ...todoItems, {
+        id: Date.now(),
+        todo: todoText,
+      },
+    ]);
+
+    setTodoText('');
+  };
+
+  const handleDelete = (todoId) => {
+    setTodoItems(
+      todoItems.filter((item) => item.id !== todoId),
+    );
+  };
+
   return (
-    <TodoPage />
+    <div>
+      <p>assignment2</p>
+      <h3>To-do</h3>
+      <TodoForm
+        onSubmit={handleSubmit}
+        todoText={todoText}
+        handleChange={handleChange}
+      />
+      <Todos todoItems={todoItems} onClick={handleDelete} />
+    </div>
   );
 }
 
--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -3,14 +3,9 @@ function TodoForm({
   todoText,
   handleChange,
 }) {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onSubmit(todoText);
-  };
-
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={onSubmit}>
         <input
           type="text"
           placeholder="할 일을 입력해주세요."
