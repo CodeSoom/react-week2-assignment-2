259cf9aa0ce4215a68345392de48b6a0090eb125
sejineeee <sejinee0918@gmail.com>
2022-10-23 14:54:17 +0900

    refactor: 삼항연산자를 if 구문으로 변경
    삼항연산자를 이용한 로직을 if 구문으로 변경

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,11 +1,13 @@
 import TodoItem from './TodoItem';
 
 function TodoList({ todoList, onDelete }) {
+  if (todoList.length === 0) {
+    return <p>할 일이 없어요!</p>;
+  }
   return (
     <div>
-      {todoList.length === 0
-        ? <p>할 일이 없어요!</p>
-        : todoList.map((item, i) => (
+      {
+        todoList.map((item, i) => (
           <TodoItem
             todoItem={item}
             index={i}
@@ -13,7 +15,8 @@ function TodoList({ todoList, onDelete }) {
             key={item.id}
             onDelete={onDelete}
           />
-        ))}
+        ))
+      }
     </div>
   );
 }
