cef7add6488bcc1316a3c6c0927343c1d06135c2
Jason <jasondal91@gmail.com>
2022-12-16 19:46:27 +0900

    List 컴포넌트 makeTodoList 함수 제거

--- src/list.jsx
+++ src/list.jsx
@@ -1,13 +1,19 @@
-const makeTodoList = (todos = [], onClick) => {
-  return todos.map((todo, index) => (
-    <li key={index}>
-      {todo} <button onClick={() => onClick(index)}> 완료</button>
-    </li>
-  ));
-};
-
 const List = ({ todos = [], onClick }) => {
-  return <>{todos.length ? <ol>{makeTodoList(todos, onClick)}</ol> : <p>할 일이 없어요!</p>}</>;
+  return (
+    <>
+      {todos.length ? (
+        <ol>
+          {todos.map((todo, index) => (
+            <li key={index}>
+              {todo} <button onClick={() => onClick(index)}> 완료</button>
+            </li>
+          ))}
+        </ol>
+      ) : (
+        <p>할 일이 없어요!</p>
+      )}
+    </>
+  );
 };
 
 export default List;
