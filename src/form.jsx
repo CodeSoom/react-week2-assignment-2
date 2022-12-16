a745a0c811d07b5430811fa4646dba2fc60fe9e5
Jason <jasondal91@gmail.com>
2022-12-16 19:47:16 +0900

    Form 컴포넌트 button type 추가

--- src/form.jsx
+++ src/form.jsx
@@ -2,7 +2,7 @@ const Form = ({ value, onSubmit, onChange }) => {
   return (
     <form onSubmit={onSubmit}>
       <input type='text' value={value} onChange={onChange} placeholder={'할 일을 입력해주세요'} />
-      <button>추가</button>
+      <button type='submit'>추가</button>
     </form>
   );
 };
