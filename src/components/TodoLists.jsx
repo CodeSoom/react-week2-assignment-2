515cc000e0b66d42a74d6b549546a4b5e75a3ecb
ctaaag <qudxor102@gmail.com>
2023-05-12 21:00:28 +0900

    fix: 할일이 없을 때 추가

--- src/components/TodoLists.jsx
+++ src/components/TodoLists.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 
 export default function TodoLists({ todos, onClickComplete }) {
-  return (
-    <ol>
-      {todos?.map((item) => (
-        <li key={item.id}>
-          <span>{item.text}</span>
-          <button type="button" id={item.id} onClick={onClickComplete}>
-            완료
-          </button>
-        </li>
-      ))}
-    </ol>
-  );
+  const showTodos = () => {
+    if (todos.length === 0) {
+      return <span>할 일이 없어요!</span>;
+    }
+    return todos.map((item) => (
+      <li key={item.id}>
+        <span>{item.text}</span>
+        <button type="button" id={item.id} onClick={onClickComplete}>
+          완료
+        </button>
+      </li>
+    ));
+  };
+
+  return <ol>{showTodos()}</ol>;
 }
